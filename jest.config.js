1dc5f6ad9e62dc464ba07d4972c4e2c70dd45784
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-12-10 11:00:28 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,18 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/node_modules/**'],
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
-    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$'
   ],
-  setupFilesAfterEnv: ['./jest.setup.js']
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__']
 }
