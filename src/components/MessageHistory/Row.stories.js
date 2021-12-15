0c7489dd97cc53193c6a3cba444cf1a0e0b0e722
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-12-15 14:02:33 -0500

    Wire up basics for MsgHistory V2

---
+++ src/components/MessageHistory/Row.stories.js
@@ -0,0 +1,64 @@
+import theme from '../theme'
+import ThemeProvider from '../ThemeProvider'
+
+import Row from './Row'
+
+export default {
+  title: 'MessageHistory/Row',
+  component: Row,
+  decorators: [
+    Story => (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
+      </div>
+    )
+  ],
+  parameters: { actions: { argTypesRegex: '^on.*' } }
+}
+
+const Template = args => (
+  <Row
+    {...args}
+    addressHref={address => `/#/history/${address}`}
+    cidHref={cid => `/#/detail/${cid}`}
+  />
+)
+
+export const GenericRow = Template.bind({})
+GenericRow.args = {
+  cid: 'bafy2bzaceaspwtvvz3hf23gw5eu2547cl2kwdruqt3omnow5ukazi3aopmmjm',
+  method: 'transfer',
+  height: '234125',
+  timestamp: '23516427',
+  from: 'f1b64l7up4oov74ignqfadshkge7h7aifwsaapjzd',
+  to: 'f1b64l7up4oov74ignqfadshkge7h7aivdsjiownio',
+  value: '13547168198490157',
+  feePaid: '21375685904209'
+}
+
+export const SearchedRow = Template.bind({})
+SearchedRow.args = {
+  cid: 'bafy2bzaceaspwtvvz3hf23gw5eu2547cl2kwdruqt3omnow5ukazi3aopmmjm',
+  method: 'transfer',
+  height: '234125',
+  age: '23516427',
+  from: 'f1b64l7up4oov74ignqfadshkge7h7aifwsaapjzd',
+  to: 'f1b64l7up4oov74ignqfadshkge7h7aivdsjiownio',
+  value: '13547168198490157',
+  fee: '21375685904209',
+  inspectingAddress: 'f1b64l7up4oov74ignqfadshkge7h7aifwsaapjzd'
+}
+
+export const PendingRow = Template.bind({})
+PendingRow.args = {
+  cid: 'bafy2bzaceaspwtvvz3hf23gw5eu2547cl2kwdruqt3omnow5ukazi3aopmmjm',
+  method: 'transfer',
+  height: '234125',
+  age: '23516427',
+  from: 'f1b64l7up4oov74ignqfadshkge7h7aifwsaapjzd',
+  to: 'f1b64l7up4oov74ignqfadshkge7h7aivdsjiownio',
+  value: '13547168198490157',
+  fee: '21375685904209',
+  inspectingAddress: 'f1b64l7up4oov74ignqfadshkge7h7aifwsaapjzd',
+  pending: true
+}
