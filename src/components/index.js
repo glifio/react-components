a18a5972f262ed25b1db0c178e388cb3cac26e3d
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-12-14 21:20:09 -0500

    Delete faulty import

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as theme } from './theme'
 export * from './theme'
 export { default as ThemeProvider } from './ThemeProvider'
 
-export { default as Container } from './Container'
 export { default as Card } from './Card'
 export { default as OnboardCard } from './Card/OnboardCard'
 export { default as AccountCard } from './AccountCard'
