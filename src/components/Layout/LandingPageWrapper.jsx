80cc5756397bf087428869565ae444f05f18bb92
Thijs van Hoof <thijs.v.h@gmail.com>
2022-01-21 21:46:05 +0100

    Update landing page styling

--- src/components/Layout/LandingPageWrapper.jsx
+++ src/components/Layout/LandingPageWrapper.jsx
@@ -2,43 +2,21 @@ import styled, { css } from 'styled-components'
 import { devices, space } from '../theme'
 
 export const LandingPageContainer = styled.div`
-  min-height: 100%;
   display: flex;
   flex-direction: column;
-  flex: 1;
+  gap: ${space()};
+  margin: ${space()};
 `
 
 export const LandingPageContentContainer = styled.div`
-  padding: ${space()};
-  padding-bottom: 0;
-  gap: ${space()};
   display: grid;
+  gap: ${space()};
 
   @media (min-width: ${devices.tablet}) {
-    min-height: 100vh;
     grid-template-columns: 1fr 1fr;
-
-    ${props =>
-      !props.phishingBannerClosed &&
-      css`
-        grid-template-rows: 50px auto;
-        grid-template-areas:
-          'banner banner'
-          'icon content';
-      `}
   }
 
   @media (max-width: ${devices.tablet}) {
     grid-template-columns: 1fr;
-
-    ${props =>
-      !props.phishingBannerClosed &&
-      css`
-        grid-template-rows: auto;
-        grid-template-areas:
-          'banner'
-          'icon'
-          'content';
-      `}
   }
 `
