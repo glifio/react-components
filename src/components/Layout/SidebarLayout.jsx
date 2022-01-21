43ec4cd38e3df9ba064eda90be49aec47cb3c0e3
Thijs van Hoof <thijs.v.h@gmail.com>
2022-01-21 23:02:25 +0100

    Simplify wallet history table layout

--- src/components/Layout/SidebarLayout.jsx
+++ src/components/Layout/SidebarLayout.jsx
@@ -1,20 +1,16 @@
 import styled from 'styled-components'
+import { space } from '../theme'
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  gap: ${space('large')};
+  margin: ${space()};
 `
 
 export const Sidebar = styled.div`
-  flex-grow: 1;
-  margin: 0.5rem 0.5rem 0.5rem 0.5rem;
+  flex: 0 0;
 `
+
 export const Content = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-basis: 0;
-  flex-grow: 999;
-  padding-top: ${props => props.theme.sizes[3]}px;
-  margin: 0 0.5rem 0.5rem 0.5rem;
-  min-width: 53%;
+  flex: 1 0;
 `
