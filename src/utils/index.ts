6ebb990cfd23479792eebb2a5c3f6415d4c13ddb
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-02-04 21:41:46 -0500

    Add actor state component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as isMobileOrTablet } from './isMobileOrTablet'
 export { default as useDesktopBrowser } from './useDesktopBrowser'
 export { default as useChromeDesktopBrowser } from './useChromeDesktopBrowser'
+export * from './useStateReadState'
 export * from './actorCode'
 export * from './isAddrEqual'
