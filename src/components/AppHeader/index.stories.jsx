d1d12c416cb27c8f22f0870bf902f79e47ad608d
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-24 17:54:10 +0100

    Add AppHeader story

--- src/components/AppHeader/index.stories.jsx
+++ src/components/AppHeader/index.stories.jsx
@@ -1,6 +1,8 @@
 import theme from '../theme'
 import ThemeProvider from '../ThemeProvider'
 import AppHeader from './index'
+import { SafeIconHeaderFooter } from '../Icons'
+import { NetworkConnection } from '../NetworkConnection'
 
 export default {
   title: 'AppHeader/AppHeader',
@@ -21,4 +23,43 @@ Base.args = {
   walletUrl: 'https://wallet.glif.io',
   verifierUrl: 'https://verifier.glif.io',
   explorerUrl: 'https://explorer.glif.io'
+}
+
+export const App = Template.bind({})
+App.args = {
+  back: () => {},
+  logout: () => {},
+  connection: (
+    <NetworkConnection
+      lotusApiAddr='https://calibration.node.glif.io'
+      apiKey='m787669344-2a9b90eb03dbff3e503c93c7'
+      statusApiAddr='https://api.uptimerobot.com/v2/getMonitors'
+      errorCallback={() => {}}
+    />
+  ),
+  appTitle: 'Safe',
+  appIcon: <SafeIconHeaderFooter />,
+  appUrl: 'https://safe.glif.io',
+  homeUrl: 'https://glif.io',
+  blogUrl: 'https://glif.io/blog',
+  walletUrl: 'https://wallet.glif.io',
+  explorerUrl: 'https://explorer.glif.io',
+  appHeaderLinks: [
+    {
+      title: 'Assets',
+      url: 'https://safe.glif.io/home'
+    },
+    {
+      title: 'History',
+      url: 'https://safe.glif.io/history'
+    },
+    {
+      title: 'Proposals',
+      url: 'https://safe.glif.io/proposals'
+    },
+    {
+      title: 'Admin',
+      url: 'https://safe.glif.io/admin'
+    }
+  ]
 }
