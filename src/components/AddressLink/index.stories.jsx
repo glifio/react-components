b94fbbce2e4220f7fba07a5566709662fffe38da
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-10 17:54:03 +0100

    Add AddressLink stories

--- src/components/AddressLink/index.stories.jsx
+++ src/components/AddressLink/index.stories.jsx
@@ -17,8 +17,24 @@ export default {
 
 const Template = args => <AddressLink {...args} />
 
-export const Base = Template.bind({})
-Base.args = {
+export const Labelled = Template.bind({})
+Labelled.args = {
   label: 'Safe Address',
   address: 'f17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy'
+}
+
+export const AddressOnly = Template.bind({})
+AddressOnly.args = {
+  address: 'f17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy'
+}
+
+export const IDOnly = Template.bind({})
+IDOnly.args = {
+  id: 't032430'
+}
+
+export const AddressAndID = Template.bind({})
+AddressAndID.args = {
+  id: 't032430',
+  address: 't1dywbadna5yyf546mloeoc7gxrzj7n5uog6llv5y'
 }
