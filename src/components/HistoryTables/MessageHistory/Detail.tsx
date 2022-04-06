f5139fd94c0132d7d2df355430e886bfeaeb14ea
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-06 20:00:22 +0200

    Pass speedUpHref and cancelHref in detail view

--- src/components/HistoryTables/MessageHistory/Detail.tsx
+++ src/components/HistoryTables/MessageHistory/Detail.tsx
@@ -42,7 +42,7 @@ const SeeMore = styled(P).attrs(() => ({
 `
 
 export default function MessageDetail(props: MessageDetailProps) {
-  const { cid, height, speedUp, cancel, confirmations } = props
+  const { cid, height, speedUpHref, cancelHref, confirmations } = props
   const time = useMemo(() => Date.now(), [])
   const [seeMore, setSeeMore] = useState(false)
   const { message, error, loading, pending } = useMessage(cid, height)
@@ -89,8 +89,8 @@ export default function MessageDetail(props: MessageDetailProps) {
       <Head
         title='Message Overview'
         pending={pending}
-        speedUp={speedUp}
-        cancel={cancel}
+        speedUpHref={speedUpHref}
+        cancelHref={cancelHref}
       />
       <hr />
       <DetailCaption
@@ -220,16 +220,16 @@ export default function MessageDetail(props: MessageDetailProps) {
 type MessageDetailProps = {
   cid: string
   height?: number
-  speedUp?: () => void
-  cancel?: () => void
+  speedUpHref?: string
+  cancelHref?: string
   confirmations: number
 }
 
 MessageDetail.propTypes = {
   cid: PropTypes.string.isRequired,
   height: PropTypes.number,
-  speedUp: PropTypes.func,
-  cancel: PropTypes.func,
+  speedUpHref: PropTypes.string,
+  cancelHref: PropTypes.string,
   confirmations: PropTypes.number
 }
 
