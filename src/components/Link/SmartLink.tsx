9e04cfcfa14367f5a07b67d949d9356f63b46b64
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-06 12:41:59 +0200

    Explicitly require SmartLink props

--- src/components/Link/SmartLink.tsx
+++ src/components/Link/SmartLink.tsx
@@ -1,25 +1,29 @@
-import React, { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
+import PropTypes from 'prop-types'
 import Link from 'next/link'
 
 // uses next/link for internal page routing
 // uses <a> tag for external page routing
-export function SmartLink(
-  props: DetailedHTMLProps<
-    AnchorHTMLAttributes<HTMLAnchorElement>,
-    HTMLAnchorElement
-  >
-) {
-  if (props?.href.charAt(0) === '/') {
-    return (
-      <Link href={props.href}>
-        <a {...props}>{props.children}</a>
-      </Link>
-    )
-  }
-
-  return (
-    <a href={props.href} target='_blank' rel='noreferrer noopener' {...props}>
-      {props.children}
+export const SmartLink = ({ href, children }: SmartLinkProps) => {
+  return href.charAt(0) === '/' ? (
+    <Link href={href}>
+      <a>{children}</a>
+    </Link>
+  ) : (
+    <a href={href} target='_blank' rel='noreferrer noopener'>
+      {children}
     </a>
   )
+}
+
+interface SmartLinkProps {
+  href: string
+  children: JSX.Element | Array<JSX.Element>
+}
+
+SmartLink.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
 }
