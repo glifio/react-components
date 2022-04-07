13825fff28b236cad5c41894beb984017c5d7849
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-07 16:51:56 +0200

    Create Dialog component

---
+++ src/components/Layout/Dialogs.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { space } from '../theme'
+
+export const Dialog = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${space()};
+  max-width: 35em;
+`
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 export * from './Boxes'
 export * from './Buttons'
 export * from './Columns'
+export * from './Dialogs'
 export * from './LandingPage'
 export * from './Page'
