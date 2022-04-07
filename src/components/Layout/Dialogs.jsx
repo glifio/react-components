e744f8d865b2441d0c04ba2ac87433b492e11a49
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-07 23:37:51 +0200

    Add form styling to dialog

--- src/components/Layout/Dialogs.jsx
+++ src/components/Layout/Dialogs.jsx
@@ -7,4 +7,20 @@ export const Dialog = styled.div`
   gap: ${space()};
   width: 100%;
   max-width: 35em;
+
+  form {
+    display: grid;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+    gap: ${space()};
+
+    label {
+      text-align: left;
+    }
+
+    input,
+    select {
+      text-align: right;
+    }
+  }
 `
