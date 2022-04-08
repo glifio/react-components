ec89953a0b5b08f7532cfe1186dc03c9e62f1d62
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-08 10:40:49 +0200

    Create Toggle.tsx

---
+++ src/components/InputV2/Toggle.tsx
@@ -0,0 +1,91 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { space } from '../theme'
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${space()};
+  cursor: pointer;
+
+  &.disabled {
+    pointer-events: none;
+
+    .toggle {
+      border-color: var(--gray-light);
+      background-color: var(--gray-light);
+    }
+  }
+
+  .wrapper {
+    display: inline-block;
+    position: relative;
+  }
+
+  input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  
+  .toggle {
+    display: block;
+    position: relative;
+    width: 2.25em;
+    height: 1.5em;
+    border-radius: 0.75em;
+    border: 2px solid var(--blue-light);
+    background-color: var(--blue-light);
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: calc(1.5em - 4px);
+      height: calc(1.5em - 4px);
+      border-radius: 50%;
+      background-color: var(--white);
+    }
+  }
+
+  &:hover .toggle,
+  input:checked ~ .toggle {
+    border-color: var(--blue-medium);
+    background-color: var(--blue-medium);
+  }
+
+  input:checked ~ .toggle::after {
+    left: auto;
+    right: 0;
+  }
+`
+
+export const Toggle = ({ label, disabled }: ToggleProps) => (
+  <Label className={disabled ? 'disabled' : ''}>
+    {label && <span>{label}</span>}
+    <span className="wrapper">
+      <input type="checkbox" />
+      <span className="toggle"></span>
+    </span>
+  </Label>
+)
+
+interface ToggleProps {
+  label: string,
+  disabled: boolean
+}
+
+Toggle.propTypes = {
+  label: PropTypes.string,
+  disabled: PropTypes.bool
+}
+
+Toggle.defaultProps = {
+  label: '',
+  disabled: false
+}
