c8e0e08d79469650f5ebb5119158c21f895d43b5
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-13 20:38:07 +0200

    Allow getting minimum gas fees from useGetReplaceMessageGasParams

--- src/utils/useGetReplaceMessageGasParams/index.ts
+++ src/utils/useGetReplaceMessageGasParams/index.ts
@@ -16,19 +16,22 @@ interface UseGetReplaceMessageGasParamsResult {
 
 export const useGetReplaceMessageGasParams = (
   provider: Filecoin,
-  message?: LotusMessage
+  message?: LotusMessage,
+  minimum?: boolean
 ): UseGetReplaceMessageGasParamsResult => {
   const [gasParams, setGasParams] = useState<GasParams | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<Error | null>(null)
+  const resolver = minimum
+    ? provider.getReplaceMessageMinGasParams
+    : provider.getReplaceMessageGasParams;
 
   useEffect(() => {
     setGasParams(null)
     setError(null)
     if (message) {
       setLoading(true)
-      provider
-        .getReplaceMessageGasParams(message)
+      resolver(message)
         .then((g: GasParams) => setGasParams(g))
         .catch((e: Error) => setError(e))
         .finally(() => setLoading(false))
