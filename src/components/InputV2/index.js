49c3d8468f009a08d5abc9521ededd0e24084396
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-14 00:07:01 +0200

    Export BigInt and Number inputs

--- src/components/InputV2/index.js
+++ src/components/InputV2/index.js
@@ -1,7 +1,11 @@
+import { BigIntInput } from './BigInt'
+import { NumberInput } from './Number'
 import { TextInput } from './Text'
 import { Toggle } from './Toggle'
 
 export default {
+  BigInt: BigIntInput,
+  Number: NumberInput,
   Text: TextInput,
   Toggle
 }
