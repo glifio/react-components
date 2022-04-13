b0599afb085d99ac4af9509c7821517c8c201df4
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-14 01:49:48 +0200

    Export AttoFil and Filecoin inputs

--- src/components/InputV2/index.js
+++ src/components/InputV2/index.js
@@ -1,10 +1,14 @@
+import { AttoFilInput } from './AttoFil'
 import { BigIntInput } from './BigInt'
+import { FilecoinInput } from './Filecoin'
 import { NumberInput } from './Number'
 import { TextInput } from './Text'
 import { Toggle } from './Toggle'
 
 export default {
+  AttoFil: AttoFilInput,
   BigInt: BigIntInput,
+  Filecoin: FilecoinInput,
   Number: NumberInput,
   Text: TextInput,
   Toggle
