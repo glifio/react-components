0af2090e8cf87a2448ad3fb8a3187468806da8fe
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-13 21:29:28 +0200

    Create Number.stories.jsx

---
+++ src/components/InputV2/Number.stories.jsx
@@ -0,0 +1,28 @@
+import { Number } from './Number'
+import theme from '../theme'
+import ThemeProvider from '../ThemeProvider'
+
+export default {
+  title: 'InputV2/Number',
+  component: Number,
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '35em', margin: '0 auto' }}>
+        <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
+      </div>
+    )
+  ],
+  parameters: { actions: { argTypesRegex: '^on.*' } }
+}
+
+const Template = args => <Number {...args} />
+
+const sharedArgs = {
+  autofocus: true
+}
+
+export const Base = Template.bind({})
+Base.args = {
+  ...sharedArgs,
+  label: 'Enter your age'
+}
