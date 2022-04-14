7d1109cb9d7b3400aafa2420df65ca189c7427e4
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-14 17:17:53 +0200

    Update Filecoin.stories.jsx

--- src/components/InputV2/Filecoin.stories.jsx
+++ src/components/InputV2/Filecoin.stories.jsx
@@ -1,3 +1,4 @@
+import { FilecoinNumber } from '@glif/filecoin-number'
 import { FilecoinInput } from './Filecoin'
 import theme from '../theme'
 import ThemeProvider from '../ThemeProvider'
@@ -15,7 +16,13 @@ export default {
   parameters: { actions: { argTypesRegex: '^on.*' } }
 }
 
-const Template = args => <FilecoinInput {...args} />
+const Template = args => (
+  <FilecoinInput
+    min={new FilecoinNumber('0', 'fil')}
+    max={new FilecoinNumber('100', 'fil')}
+    {...args}
+  />
+)
 
 const sharedArgs = {
   autofocus: true,
@@ -25,5 +32,22 @@ const sharedArgs = {
 export const Base = Template.bind({})
 Base.args = {
   ...sharedArgs,
-  label: 'Enter an amount'
+  label: 'Enter an amount in FIL',
+  info: 'Must be between 0 and 100 FIL'
+}
+
+export const AttoFIL = Template.bind({})
+AttoFIL.args = {
+  ...sharedArgs,
+  label: 'Enter an amount in attoFIL',
+  info: 'Must be between 0 and 100 FIL',
+  denom: 'attofil'
+}
+
+export const PicoFIL = Template.bind({})
+PicoFIL.args = {
+  ...sharedArgs,
+  label: 'Enter an amount in picoFIL',
+  info: 'Must be between 0 and 100 FIL',
+  denom: 'picofil'
 }
