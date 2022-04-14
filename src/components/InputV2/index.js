1e33fd2c580a838e72be8b3317cf467912ff66c8
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-14 17:13:08 +0200

    Remove AttoFil input

--- src/components/InputV2/index.js
+++ src/components/InputV2/index.js
@@ -1,4 +1,3 @@
-import { AttoFilInput } from './AttoFil'
 import { BigIntInput } from './BigInt'
 import { FilecoinInput } from './Filecoin'
 import { NumberInput } from './Number'
@@ -6,7 +5,6 @@ import { TextInput, EmailInput, PasswordInput, SearchInput } from './Text'
 import { Toggle } from './Toggle'
 
 export default {
-  AttoFil: AttoFilInput,
   BigInt: BigIntInput,
   Filecoin: FilecoinInput,
   Number: NumberInput,
