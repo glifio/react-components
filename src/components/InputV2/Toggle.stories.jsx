6820047e782bf2b9f3ed8db300d93ea6837f219b
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-20 11:32:31 +0200

    Update Toggle.stories.jsx

--- src/components/InputV2/Toggle.stories.jsx
+++ src/components/InputV2/Toggle.stories.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import { Toggle } from './Toggle'
 import theme from '../theme'
 import ThemeProvider from '../ThemeProvider'
 
+const StoryComponent = ({ checked: defaultChecked, ...props }) => {
+  const [checked, setChecked] = useState(defaultChecked)
+  return (
+    <Toggle
+      checked={checked}
+      onChange={setChecked}
+      autofocus={true}
+      {...props}
+    />
+  )
+}
+
 export default {
   title: 'InputV2/Toggle',
-  component: Toggle,
+  component: StoryComponent,
   decorators: [
     Story => (
       <div style={{ maxWidth: '35em', margin: '0 auto' }}>
@@ -15,43 +28,33 @@ export default {
   parameters: { actions: { argTypesRegex: '^on.*' } }
 }
 
-const Template = args => <Toggle {...args} />
-
-const sharedArgs = {
-  autofocus: true,
-  controlled: false
-}
+const Template = args => <StoryComponent {...args} />
 
 export const Base = Template.bind({})
 Base.args = {
-  ...sharedArgs,
   label: 'Toggle me'
 }
 
 export const Checked = Template.bind({})
 Checked.args = {
-  ...sharedArgs,
   label: 'Toggle me',
   checked: true
 }
 
 export const WithInfo = Template.bind({})
 WithInfo.args = {
-  ...sharedArgs,
   label: 'Toggle me',
   info: "You won't be disappointed"
 }
 
 export const Disabled = Template.bind({})
 Disabled.args = {
-  ...sharedArgs,
   label: 'You cannot toggle me',
   disabled: true
 }
 
 export const DisabledChecked = Template.bind({})
 DisabledChecked.args = {
-  ...sharedArgs,
   label: 'You cannot toggle me',
   disabled: true,
   checked: true
