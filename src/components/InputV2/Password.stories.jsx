ef49030d4f2bdaf4710c58bb49c93d83586793ce
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-20 11:43:26 +0200

    Update Password.stories.jsx

--- src/components/InputV2/Password.stories.jsx
+++ src/components/InputV2/Password.stories.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import { PasswordInput } from './Text'
 import theme from '../theme'
 import ThemeProvider from '../ThemeProvider'
 
+const StoryComponent = ({ value: defaultValue, ...props }) => {
+  const [value, setValue] = useState(defaultValue)
+  return (
+    <PasswordInput
+      value={value}
+      onChange={setValue}
+      autofocus={true}
+      {...props}
+    />
+  )
+}
+
 export default {
   title: 'InputV2/Password',
-  component: PasswordInput,
+  component: StoryComponent,
   decorators: [
     Story => (
       <div style={{ maxWidth: '35em', margin: '0 auto' }}>
@@ -15,22 +28,15 @@ export default {
   parameters: { actions: { argTypesRegex: '^on.*' } }
 }
 
-const Template = args => <PasswordInput {...args} />
-
-const sharedArgs = {
-  autofocus: true,
-  controlled: false
-}
+const Template = args => <StoryComponent {...args} />
 
 export const Base = Template.bind({})
 Base.args = {
-  ...sharedArgs,
   label: 'Enter your password'
 }
 
 export const Disabled = Template.bind({})
 Disabled.args = {
-  ...sharedArgs,
   label: 'You cannot enter your password',
   disabled: true,
   value: 'Too bad'
