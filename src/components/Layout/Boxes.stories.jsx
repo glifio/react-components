39dc7020b8a72243ca31b0d881466754399e7a6b
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-21 23:46:27 +0200

    Update Boxes.stories.jsx

--- src/components/Layout/Boxes.stories.jsx
+++ src/components/Layout/Boxes.stories.jsx
@@ -4,7 +4,7 @@ import { StandardBox, ShadowBox, InfoBox, WarningBox, ErrorBox } from './Boxes'
 import theme from '../theme'
 import ThemeProvider from '../ThemeProvider'
 
-const StoryComponent = () => (
+const StoryComponent = ({ headers, multiline }) => (
   <OneColumnCentered>
     <Dialog>
       <StandardBox>
@@ -17,91 +17,86 @@ const StoryComponent = () => (
         </p>
       </StandardBox>
       <StandardBox>
-        <header>Box Header</header>
+        {headers && <header>Box Header</header>}
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
           nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
           senectus et netus et malesuada fames ac turpis egestas.
         </p>
-        <hr />
-        <p>
-          Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
-          fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
-        </p>
-      </StandardBox>
-      <StandardBox>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-        nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas.
+        {multiline && (
+          <>
+            <hr />
+            <p>
+              Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
+              fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
+            </p>
+          </>
+        )}
       </StandardBox>
       <ShadowBox>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-        nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas.
-      </ShadowBox>
-      <ShadowBox>
-        <header>Box Header</header>
+        {headers && <header>Box Header</header>}
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
           nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
           senectus et netus et malesuada fames ac turpis egestas.
         </p>
-        <hr />
-        <p>
-          Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
-          fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
-        </p>
+        {multiline && (
+          <>
+            <hr />
+            <p>
+              Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
+              fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
+            </p>
+          </>
+        )}
       </ShadowBox>
-      <InfoBox>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-        nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas.
-      </InfoBox>
       <InfoBox>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
           nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
           senectus et netus et malesuada fames ac turpis egestas.
         </p>
-        <hr />
-        <p>
-          Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
-          fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
-        </p>
+        {multiline && (
+          <>
+            <hr />
+            <p>
+              Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
+              fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
+            </p>
+          </>
+        )}
       </InfoBox>
       <WarningBox>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-        nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas.
-      </WarningBox>
-      <WarningBox>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
           nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
           senectus et netus et malesuada fames ac turpis egestas.
         </p>
-        <hr />
-        <p>
-          Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
-          fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
-        </p>
+        {multiline && (
+          <>
+            <hr />
+            <p>
+              Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
+              fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
+            </p>
+          </>
+        )}
       </WarningBox>
-      <ErrorBox>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
-        nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas.
-      </ErrorBox>
       <ErrorBox>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
           nibh vitae tincidunt ultrices. Pellentesque habitant morbi tristique
           senectus et netus et malesuada fames ac turpis egestas.
         </p>
-        <hr />
-        <p>
-          Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
-          fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
-        </p>
+        {multiline && (
+          <>
+            <hr />
+            <p>
+              Donec gravida aliquam tellus ut pulvinar. Pellentesque sit amet
+              fringilla nulla. Proin dignissim venenatis turpis sed rutrum.
+            </p>
+          </>
+        )}
       </ErrorBox>
     </Dialog>
   </OneColumnCentered>
@@ -114,6 +109,23 @@ export default {
   parameters: { actions: { argTypesRegex: '^on.*' } }
 }
 
-const Template = () => <StoryComponent />
+const Template = args => <StoryComponent {...args} />
 
 export const Base = Template.bind({})
+Base.args = {}
+
+export const Headers = Template.bind({})
+Headers.args = {
+  headers: true
+}
+
+export const Multiline = Template.bind({})
+Multiline.args = {
+  multiline: true
+}
+
+export const Everything = Template.bind({})
+Everything.args = {
+  headers: true,
+  multiline: true
+}
