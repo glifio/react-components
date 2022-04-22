e9b35b6224edb88f15bacdd18359205c90ef6ed1
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-22 17:34:34 +0200

    Create TransactionMaxFee

---
+++ src/components/Transaction/MaxFee.tsx
@@ -0,0 +1,22 @@
+import { FilecoinNumber } from '@glif/filecoin-number'
+import { FILECOIN_NUMBER_PROPTYPE } from '../../customPropTypes'
+import { SmartLink } from '../Link/SmartLink'
+
+export const TransactionMaxFee = ({ maxFee }: TransactionMaxFeeProps) => {
+  return (
+    <p>
+      You will not pay more than {maxFee.toFil()} FIL for this transaction.{' '}
+      <SmartLink href='https://filfox.info/en/stats/gas'>
+        More information on average gas fee statistics.
+      </SmartLink>
+    </p>
+  )
+}
+
+interface TransactionMaxFeeProps {
+  maxFee: FilecoinNumber
+}
+
+TransactionMaxFee.propTypes = {
+  maxFee: FILECOIN_NUMBER_PROPTYPE.isRequired
+}
--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,7 +1,9 @@
 import { TransactionHeader } from './Header'
+import { TransactionMaxFee } from './MaxFee'
 import { TransactionTotal } from './Total'
 
 export const Transaction = {
   Header: TransactionHeader,
+  MaxFee: TransactionMaxFee,
   Total: TransactionTotal
 }
