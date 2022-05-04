61074a22a129863c821991f08e05421239db5f32
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-04 13:10:44 +0200

    Add gas params comparison in custom hooks

--- src/utils/useGetReplaceMessageGasParams.ts
+++ src/utils/useGetReplaceMessageGasParams.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { Message } from '@glif/filecoin-message'
 import { FilecoinNumber } from '@glif/filecoin-number'
 import Filecoin from '@glif/filecoin-wallet-provider'
@@ -25,6 +25,19 @@ export const useGetReplaceMessageGasParams = (
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<Error | null>(null)
 
+  // Store gas params in a ref so we can check if they changed
+  // in the useEffect hook below without causing an infinite loop
+  const gasParamsRef = useRef<GasParams | null>(null)
+  const didUpdate = useCallback((params: GasParams) => {
+    const update: boolean =
+      !gasParamsRef.current ||
+      !gasParamsRef.current.gasFeeCap.isEqualTo(params.gasFeeCap) ||
+      !gasParamsRef.current.gasPremium.isEqualTo(params.gasPremium) ||
+      !gasParamsRef.current.gasLimit.isEqualTo(params.gasLimit)
+    if (update) gasParamsRef.current = params
+    return update
+  }, [])
+
   useEffect(() => {
     const getGasParams = minimum
       ? provider.getReplaceMessageMinGasParams
@@ -34,17 +47,18 @@ export const useGetReplaceMessageGasParams = (
     if (message) {
       setLoading(true)
       getGasParams(message.toLotusType())
-        .then((g: ProviderGasParams) =>
-          setGasParams({
+        .then((g: ProviderGasParams) => {
+          const newGasParams = {
             gasFeeCap: new FilecoinNumber(g.gasFeeCap, 'attofil'),
             gasPremium: new FilecoinNumber(g.gasPremium, 'attofil'),
             gasLimit: new FilecoinNumber(g.gasLimit, 'attofil')
-          })
-        )
+          }
+          if (didUpdate(newGasParams)) setGasParams(newGasParams)
+        })
         .catch((e: Error) => setError(e))
         .finally(() => setLoading(false))
     }
-  }, [provider, message, minimum])
+  }, [provider, message, minimum, didUpdate])
 
   return { gasParams, loading, error }
 }
