3de1a0c6a6e2bdae3b06f8ba379256776613097f
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-12 14:52:53 +0200

    Export tx fee

--- src/components/Transaction/index.ts
+++ src/components/Transaction/index.ts
@@ -1,6 +1,7 @@
 import { TransactionBalance } from './Balance'
 import { TransactionButtons } from './Buttons'
 import { TransactionConfirm } from './Confirm'
+import { TransactionFee } from './Fee'
 import { TransactionHeader } from './Header'
 import { TransactionMaxFee } from './MaxFee'
 import { TransactionTotal } from './Total'
@@ -9,6 +10,7 @@ export const Transaction = {
   Balance: TransactionBalance,
   Buttons: TransactionButtons,
   Confirm: TransactionConfirm,
+  Fee: TransactionFee,
   Header: TransactionHeader,
   MaxFee: TransactionMaxFee,
   Total: TransactionTotal
