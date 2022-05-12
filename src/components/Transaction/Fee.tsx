3c5a97913a8d974a9dc1afecaef818a21aa8404c
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-12 14:11:22 +0200

    Update Fee.tsx

--- src/components/Transaction/Fee.tsx
+++ src/components/Transaction/Fee.tsx
@@ -1,64 +1,33 @@
 import PropTypes from 'prop-types'
-import { useEffect, useMemo, useState } from 'react'
-import { Message } from '@glif/filecoin-message'
+import { useEffect, useState } from 'react'
 import { FilecoinNumber } from '@glif/filecoin-number'
-import Filecoin from '@glif/filecoin-wallet-provider'
 
-import { getMaxAffordableFee } from '../../utils/getMaxAffordableFee'
-import { getMaxGasFee } from '../../utils/getMaxGasFee'
-import { useGetGasParams } from '../../utils/useGetGasParams'
 import { FilecoinInput } from '../InputV2/Filecoin'
 import { TransactionMaxFee } from './MaxFee'
-import {
-  Wallet,
-  WALLET_PROPTYPE,
-  FILECOIN_NUMBER_PROPTYPE
-} from '../../customPropTypes'
+import { FILECOIN_NUMBER_PROPTYPE } from '../../customPropTypes'
 
 export const TransactionFee = ({
-  provider,
-  message,
-  wallet,
-  sendAmount,
+  maxFee,
+  setMaxFee,
+  affordableFee,
+  calculatedFee,
+  gasLoading,
   disabled
 }: TransactionFeeProps) => {
   // Input states
   const [txFee, setTxFee] = useState<FilecoinNumber | null>(null)
   const [isTxFeeValid, setIsTxFeeValid] = useState<boolean>(false)
 
-  // Max transaction fee used for getting gas params. Will be
-  // null until the user manually changes the transaction fee.
-  const [maxFee, setMaxFee] = useState<FilecoinNumber | null>(null)
-
-  // Load gas parameters when message or max fee changes
-  const {
-    gasParams,
-    loading: gasParamsLoading,
-    error: gasParamsError
-  } = useGetGasParams(provider, message, maxFee)
-
-  // Calculate max affordable fee (balance minus value)
-  const maxAffordableFee = useMemo<FilecoinNumber | null>(() => {
-    return sendAmount ? getMaxAffordableFee(wallet.balance, sendAmount) : null
-  }, [sendAmount, wallet.balance])
-
-  // Calculate maximum transaction fee (fee cap times limit)
-  const calculatedMaxFee = useMemo<FilecoinNumber | null>(() => {
-    return gasParams
-      ? getMaxGasFee(gasParams.gasFeeCap, gasParams.gasLimit)
-      : null
-  }, [gasParams])
-
   // The first time we calculate a valid maximum transaction fee, we set the value
   // for the transaction fee input. Afterwards, the transaction fee input becomes
   // editable and the calculated fee is updated according to the user's input.
   const [initialFeeSet, setInitialFeeSet] = useState<boolean>(false)
   useEffect(() => {
-    if (calculatedMaxFee && !initialFeeSet) {
+    if (calculatedFee && !initialFeeSet) {
       setInitialFeeSet(true)
-      setTxFee(calculatedMaxFee)
+      setTxFee(calculatedFee)
     }
-  }, [calculatedMaxFee, initialFeeSet])
+  }, [calculatedFee, initialFeeSet])
 
   // When leaving the transaction fee input, we set maxFee to
   // update the gas params if the following conditions are met:
@@ -70,7 +39,7 @@ export const TransactionFee = ({
       txFee &&
       isTxFeeValid &&
       (!maxFee || txFee.toAttoFil() !== maxFee.toAttoFil()) &&
-      (!calculatedMaxFee || txFee.toAttoFil() !== calculatedMaxFee.toAttoFil())
+      (!calculatedFee || txFee.toAttoFil() !== calculatedFee.toAttoFil())
     ) {
       setMaxFee(txFee)
     }
@@ -81,7 +50,7 @@ export const TransactionFee = ({
       {initialFeeSet && (
         <FilecoinInput
           label='Transaction Fee'
-          max={maxAffordableFee}
+          max={affordableFee}
           value={txFee}
           denom='attofil'
           onBlur={onBlurTxFee}
@@ -90,24 +59,26 @@ export const TransactionFee = ({
           disabled={disabled}
         />
       )}
-      {gasParamsLoading && <p>Calculating transaction fees...</p>}
-      {calculatedMaxFee && <TransactionMaxFee maxFee={calculatedMaxFee} />}
+      {gasLoading && <p>Calculating transaction fees...</p>}
+      {calculatedFee && <TransactionMaxFee maxFee={calculatedFee} />}
     </>
   )
 }
 
 export interface TransactionFeeProps {
-  provider: Filecoin
-  message: Message
-  wallet: Wallet
-  sendAmount: FilecoinNumber
+  maxFee: FilecoinNumber
+  setMaxFee: (maxFee: FilecoinNumber) => void
+  affordableFee: FilecoinNumber
+  calculatedFee: FilecoinNumber
+  gasLoading: boolean
   disabled: boolean
 }
 
 TransactionFee.propTypes = {
-  provider: PropTypes.object.isRequired,
-  message: PropTypes.object.isRequired,
-  wallet: WALLET_PROPTYPE.isRequired,
-  sendAmount: FILECOIN_NUMBER_PROPTYPE.isRequired,
+  maxFee: FILECOIN_NUMBER_PROPTYPE.isRequired,
+  setMaxFee: PropTypes.func.isRequired,
+  affordableFee: FILECOIN_NUMBER_PROPTYPE.isRequired,
+  calculatedFee: FILECOIN_NUMBER_PROPTYPE.isRequired,
+  gasLoading: PropTypes.bool,
   disabled: PropTypes.bool
 }
