8d12e4481764d71e18575ac1108f55bc07015520
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-13 10:44:46 +0200

    Update Fee.tsx

--- src/components/Transaction/Fee.tsx
+++ src/components/Transaction/Fee.tsx
@@ -20,12 +20,12 @@ export const TransactionFee = ({
   const [txFee, setTxFee] = useState<FilecoinNumber | null>(null)
   const [isTxFeeValid, setIsTxFeeValid] = useState<boolean>(false)
 
-  // When leaving the transaction fee input, we set maxFee to
-  // update the gas params if the following conditions are met:
+  // When leaving the tx fee input or pressing Enter, we set maxFee
+  // to update the gas params if the following conditions are met:
   // - the input is valid
   // - the value is different from the previous max fee
   // - the value is different from the calculated max fee
-  const onBlurTxFee = () => {
+  const setMaxFeeIfChanged = () => {
     if (
       txFee &&
       isTxFeeValid &&
@@ -45,6 +45,9 @@ export const TransactionFee = ({
 
   return (
     <>
+      {/* Once we have an initially calculated
+          fee, or we are loading a new one, we
+          display the UI to modify the fee */}
       {(gasLoading || calculatedFee) && (
         <>
           <Toggle
@@ -59,7 +62,8 @@ export const TransactionFee = ({
               max={affordableFee}
               value={txFee}
               denom='attofil'
-              onBlur={onBlurTxFee}
+              onBlur={setMaxFeeIfChanged}
+              onEnter={setMaxFeeIfChanged}
               onChange={setTxFee}
               setIsValid={setIsTxFeeValid}
               disabled={gasLoading || disabled}
