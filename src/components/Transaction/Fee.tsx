a75f3ebbaa9a51e04e65c3392d26680533e959c9
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-13 11:04:20 +0200

    Update Fee.tsx

--- src/components/Transaction/Fee.tsx
+++ src/components/Transaction/Fee.tsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { FilecoinNumber } from '@glif/filecoin-number'
 
 import { Toggle } from '../InputV2/Toggle'
