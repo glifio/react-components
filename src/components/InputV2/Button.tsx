1f66167830843c122adbf16ca2a3910fda6058a2
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-20 14:55:30 +0200

    Create ButtonInput

---
+++ src/components/InputV2/Button.tsx
@@ -0,0 +1,23 @@
+import {
+  BaseInput,
+  BaseInputProps,
+  BaseInputPropTypes
+} from './Base'
+
+/**
+ * ButtonInput
+ */
+
+export const ButtonInput = (props: ButtonInputProps) => (
+  <BaseInput type='button' {...props} />
+)
+
+/**
+ * Allow the full set of base input props, except
+ * "type", which is always "button" for button input
+ */
+
+export type ButtonInputProps = Omit<BaseInputProps, 'type'>
+const { type, ...buttonInputPropTypes } = BaseInputPropTypes
+
+ButtonInput.propTypes = buttonInputPropTypes
