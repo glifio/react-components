83e80269a2ce4bd91fdc1ae40ca2e90ecffd39f7
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-20 17:30:27 +0200

    Add button ClassName utils

--- src/components/InputV2/Button.tsx
+++ src/components/InputV2/Button.tsx
@@ -1,4 +1,33 @@
+import PropTypes from 'prop-types'
 import { BaseInput, BaseInputProps, BaseInputPropTypes } from './Base'
+
+/**
+ * Button
+ */
+
+const buttonClassNames = ['disabled', 'large', 'white', 'gray', 'red', 'green']
+
+interface ButtonClassNameProps {
+  disabled?: boolean
+  large?: boolean
+  white?: boolean
+  gray?: boolean
+  red?: boolean
+  green?: boolean
+}
+
+const ButtonClassNamePropTypes = {
+  disabled: PropTypes.bool,
+  large: PropTypes.bool,
+  white: PropTypes.bool,
+  gray: PropTypes.bool,
+  red: PropTypes.bool,
+  green: PropTypes.bool
+}
+
+const getButtonClassName = (props: ButtonClassNameProps): string => {
+  return Object.keys(props).filter(name => props[name] && buttonClassNames.includes(name)).join(' ')
+}
 
 /**
  * ButtonInput
