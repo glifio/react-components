e9dcc0a97ed5d03c6860fc5b508616e3e033bc2d
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-31 18:57:37 +0200

    Create PrivateKey.stories.jsx

---
+++ src/components/InputV2/PrivateKey.stories.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { ButtonV2 } from '../Button/V2'
+import { ButtonRowRight } from '../Layout/Buttons'
+import { PrivateKeyInput } from './PrivateKey'
+import theme from '../theme'
+import ThemeProvider from '../ThemeProvider'
+
+const StoryComponent = ({ value: defaultValue, ...props }) => {
+  const [value, setValue] = useState(defaultValue)
+  const [isValid, setIsValid] = useState(false)
+  return (
+    <div>
+      <PrivateKeyInput
+        value={value}
+        onChange={setValue}
+        setIsValid={setIsValid}
+        autoFocus={true}
+        {...props}
+      />
+      <ButtonRowRight>
+        <ButtonV2 disabled={!isValid}>Send</ButtonV2>
+      </ButtonRowRight>
+    </div>
+  )
+}
+
+export default {
+  title: 'InputV2/PrivateKey',
+  component: StoryComponent,
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '35em', margin: '0 auto' }}>
+        <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
+      </div>
+    )
+  ],
+  parameters: { actions: { argTypesRegex: '^on.*' } }
+}
+
+const Template = args => <StoryComponent {...args} />
+
+export const Base = Template.bind({})
+Base.args = {
+  label: 'Enter a private key'
+}
