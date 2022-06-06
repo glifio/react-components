c65705428311511640f0caea6452cb8edd8fd98b
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-06 11:32:13 +0200

    Fix urlParams test

--- src/utils/urlParams/index.test.ts
+++ src/utils/urlParams/index.test.ts
@@ -1,35 +1,39 @@
 import { generateRouteWithRequiredUrlParams } from '.'
-import { PAGE } from '../../constants'
+
+enum PAGE {
+  LANDING = '/',
+  HOME = '/home'
+}
 
 describe('generateRouteWithRequiredUrlParams', () => {
   test('it does not change the required params when generating navigation urls', () => {
     const route = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 't' },
-      pageUrl: PAGE.MSIG_HOME
+      pageUrl: PAGE.HOME
     })
 
-    expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route.includes(PAGE.HOME)).toBe(true)
     expect(route.includes('?network=t')).toBe(true)
   })
 
   test('it allows for adding new query params', () => {
     const route = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 't' },
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       newQueryParams: { test: 'value' }
     })
 
-    expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route.includes(PAGE.HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
     expect(route.includes('test=value')).toBe(true)
 
     const route2 = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 'f' },
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       newQueryParams: { test: 'value' }
     })
 
-    expect(route2.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route2.includes(PAGE.HOME)).toBe(true)
     expect(route2.includes('network=f')).toBe(true)
     expect(route2.includes('test=value')).toBe(true)
   })
@@ -37,21 +41,21 @@ describe('generateRouteWithRequiredUrlParams', () => {
   test('it allows for adding computed paths', () => {
     const route = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 't' },
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       urlPathExtension: ['extension']
     })
 
-    expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route.includes(PAGE.HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
     expect(route.includes('/extension')).toBe(true)
 
     const route2 = generateRouteWithRequiredUrlParams({
       existingQParams: {},
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       urlPathExtension: ['extension']
     })
 
-    expect(route2.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route2.includes(PAGE.HOME)).toBe(true)
     expect(route2.includes('?')).toBe(false)
     expect(route2.includes('/extension')).toBe(true)
   })
@@ -59,7 +63,7 @@ describe('generateRouteWithRequiredUrlParams', () => {
   test('it deletes not required q params with the flag', () => {
     const route = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 't', param2: 'kobe' },
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       urlPathExtension: ['extension', 'second-extension'],
       newQueryParams: {
         test: 'value',
@@ -68,7 +72,7 @@ describe('generateRouteWithRequiredUrlParams', () => {
       maintainQueryParams: false
     })
 
-    expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route.includes(PAGE.HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(false)
     expect(route.includes('param2=kobe')).toBe(false)
     expect(route.includes('/extension/second-extension')).toBe(true)
@@ -79,7 +83,7 @@ describe('generateRouteWithRequiredUrlParams', () => {
   test('it works for the most complex cases', () => {
     const route = generateRouteWithRequiredUrlParams({
       existingQParams: { network: 't', param2: 'kobe' },
-      pageUrl: PAGE.MSIG_HOME,
+      pageUrl: PAGE.HOME,
       maintainQueryParams: true,
       urlPathExtension: ['extension', 'second-extension'],
       newQueryParams: {
@@ -88,7 +92,7 @@ describe('generateRouteWithRequiredUrlParams', () => {
       }
     })
 
-    expect(route.includes(PAGE.MSIG_HOME)).toBe(true)
+    expect(route.includes(PAGE.HOME)).toBe(true)
     expect(route.includes('network=t')).toBe(true)
     expect(route.includes('param2=kobe')).toBe(true)
     expect(route.includes('/extension/second-extension')).toBe(true)
