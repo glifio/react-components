bb9db2f05059235be840122a45264e1071f9b932
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-13 16:13:55 +0200

    Create Lines layout

---
+++ src/components/Layout/Lines.tsx
@@ -0,0 +1,96 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { AddressLink } from '../AddressLink'
+import { Address } from '../../generated/graphql'
+import { ADDRESS_PROPTYPE } from '../../customPropTypes'
+
+/**
+ * Lines
+ */
+
+export const Lines = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+
+  p,
+  hr {
+    margin: 0;
+  }
+`
+
+/**
+ * Line
+ */
+
+const LineEl = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1em;
+  line-height: 2em;
+  text-align: left;
+  padding-left: ${props => `${props.depth * 1.5}em`};
+
+  > div:first-child {
+    flex: 0 1 25%;
+    min-width: 200px;
+  }
+
+  > div:last-child {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    gap: 0.75em;
+    word-break: break-word;
+  }
+`
+
+export const Line = ({ label, depth, children }: LineProps) => (
+  <LineEl depth={depth}>
+    <div>{label}</div>
+    <div>{children}</div>
+  </LineEl>
+)
+
+type LineProps = {
+  label?: string
+  depth?: number
+  children?: React.ReactNode
+}
+
+const LinePropTypes = {
+  label: PropTypes.string,
+  depth: PropTypes.number,
+  children: PropTypes.node
+}
+
+Line.propTypes = LinePropTypes
+Line.defaultProps = {
+  label: '',
+  depth: 0,
+  children: <></>
+}
+
+/**
+ * AddressLine
+ */
+
+export const AddressLine = ({ value, label, depth }: AddressLineProps) =>
+  typeof value === 'string' ? (
+    <Line label={label} depth={depth}>
+      <AddressLink address={value} hideCopyText={false} />
+    </Line>
+  ) : (
+    <Line label={label} depth={depth}>
+      <AddressLink id={value.id} address={value.robust} hideCopyText={false} />
+    </Line>
+  )
+
+type AddressLineProps = {
+  value: string | Address
+} & LineProps
+
+AddressLine.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, ADDRESS_PROPTYPE]).isRequired,
+  ...LinePropTypes
+}
