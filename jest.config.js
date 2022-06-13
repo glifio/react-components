ac5b7142804345d8bfd6097702f12017f386ca43
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-06-13 13:23:56 -0700

    Fix tests with moduleNameMapping

--- jest.config.js
+++ jest.config.js
@@ -7,14 +7,17 @@ module.exports = {
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$'
   ],
   moduleNameMapper: {
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    'multiformats/cid': '<rootDir>/node_modules/multiformats/cjs/src/cid.js',
+    '@ipld/dag-cbor': '<rootDir>/node_modules/@ipld/dag-cbor/cjs/index.js',
+    cborg: '<rootDir>/node_modules/cborg/cjs/cborg.js'
   },
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
