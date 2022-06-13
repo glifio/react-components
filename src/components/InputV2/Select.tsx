1c5dea6c59855aa7ab822dc9f00dc3117c3db0f8
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-13 18:55:38 +0200

    Add missing import

--- src/components/InputV2/Select.tsx
+++ src/components/InputV2/Select.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
 import { Label } from './Label'
