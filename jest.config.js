1fcf8be7931ce7d0d09bacbbc4eeb9b41c12b4c1
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-14 10:35:00 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,24 +2,15 @@
 module.exports = {
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
-    '!**/node_modules/**',
-    '!src/**/*.stories.{js,jsx,ts,tsx}'
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!**/node_modules/**'
   ],
+  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$'
-  ],
-  moduleNameMapper: {
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    'multiformats/cid': '<rootDir>/node_modules/multiformats/cjs/src/cid.js',
-    '@ipld/dag-cbor': '<rootDir>/node_modules/@ipld/dag-cbor/cjs/index.js',
-    cborg: '<rootDir>/node_modules/cborg/cjs/cborg.js'
+  transform: {},
+  jest: {
+    extensionsToTreatAsEsm: ['.ts']
   },
   setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'jsdom',
-  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__']
+  testEnvironment: 'jsdom'
 }
