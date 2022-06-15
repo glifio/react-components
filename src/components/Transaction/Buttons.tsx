45c133b635e0dfcc6979c32bafb6ed89a69db4cc
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-15 12:52:20 +0200

    fix button types

--- src/components/Transaction/Buttons.tsx
+++ src/components/Transaction/Buttons.tsx
@@ -8,8 +8,6 @@ export const TransactionButtons = ({
   nextDisabled,
   backText,
   nextText,
-  backType,
-  nextType,
   onClickBack,
   onClickNext
 }: TransactionButtonsProps) => {
@@ -17,14 +15,14 @@ export const TransactionButtons = ({
   const onBack = onClickBack ? onClickBack : router ? router.back : () => {}
   return (
     <ButtonRowSpaced>
-      <ButtonV2 large disabled={backDisabled} type={backType} onClick={onBack}>
+      <ButtonV2 large type='button' disabled={backDisabled} onClick={onBack}>
         {backText}
       </ButtonV2>
       <ButtonV2
         large
         green
+        type='submit'
         disabled={nextDisabled}
-        type={nextType}
         onClick={onClickNext}
       >
         {nextText}
@@ -38,8 +36,6 @@ export interface TransactionButtonsProps {
   nextDisabled?: boolean
   backText?: string
   nextText?: string
-  backType?: string
-  nextType?: string
   onClickBack?: () => void
   onClickNext?: () => void
 }
@@ -49,8 +45,6 @@ TransactionButtons.propTypes = {
   nextDisabled: PropTypes.bool,
   backText: PropTypes.string,
   nextText: PropTypes.string,
-  backType: PropTypes.string,
-  nextType: PropTypes.string,
   onClickBack: PropTypes.func,
   onClickNext: PropTypes.func
 }
@@ -59,7 +53,5 @@ TransactionButtons.defaultProps = {
   backDisabled: false,
   nextDisabled: false,
   backText: 'Back',
-  nextText: 'Next',
-  backType: 'button',
-  nextType: 'submit'
+  nextText: 'Next'
 }
