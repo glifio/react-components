872bb3e65ea93c1b77e8ab8913e9abd729a33a20
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-20 13:51:51 +0200

    Differentiate between invalid and unknown actor codes

--- src/utils/decodeActorCID.ts
+++ src/utils/decodeActorCID.ts
@@ -1,14 +1,15 @@
 import base32Decode from 'base32-decode'
 
 export const decodeActorCID = (cid: string): string => {
+  let actorCode = ''
   try {
     const decoded = base32Decode(cid.slice(1).toUpperCase(), 'RFC4648')
-    const actorCode = new TextDecoder('utf-8').decode(decoded.slice(4))
-    if (!actorCode.includes('fil/')) {
-      throw new Error('unknown actor code')
-    }
-    return actorCode
-  } catch {
+    actorCode = new TextDecoder('utf-8').decode(decoded.slice(4))
+  } catch(e) {
     throw new Error('invalid actor code')
   }
+  if (!actorCode.includes('fil/')) {
+    throw new Error('unknown actor code')
+  }
+  return actorCode
 }
