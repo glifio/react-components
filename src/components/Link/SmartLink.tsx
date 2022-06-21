b732db4c5bb74986af4dedd975fb65b250b2958c
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-21 18:24:50 +0200

    Add parameter retention in SmartLink

--- src/components/Link/SmartLink.tsx
+++ src/components/Link/SmartLink.tsx
@@ -1,6 +1,9 @@
-import { ReactNode } from 'react'
+import { useMemo, ReactNode } from 'react'
+import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
+
+const absoluteUrlRegex = new RegExp('^(?:[a-z]+:)?//', 'i')
 
 // uses next/link for internal page routing
 // uses <a> tag for external page routing
@@ -9,25 +12,56 @@ export function SmartLink({
   href,
   download,
   className,
+  retainExtraParams,
+  retainDefaultParams,
   onClick
 }: SmartLinkProps) {
-  return href?.charAt(0) === '/' ? (
-    <Link href={href}>
-      <a className={className} onClick={onClick}>
-        {children}
-      </a>
-    </Link>
-  ) : (
+  const { query } = useRouter()
+
+  const isAbsolute = useMemo<boolean>(() => absoluteUrlRegex.test(href), [href])
+  const defaultParams = ['address', 'msigAddress']
+  const retainParams = [
+    ...(retainDefaultParams ? defaultParams : []),
+    ...(retainExtraParams ? retainExtraParams : [])
+  ]
+  const hrefWithParams = useMemo<string>(() => {
+    // Don't alter absolute URLs or when not retaining params
+    if (isAbsolute || !retainParams.length) return href
+
+    // Get query params from passed href
+    const [path, paramString] = href.split('?')
+
+    // Create parameters object
+    const params = new URLSearchParams(paramString)
+
+    // Add retained parameters
+    retainParams.forEach(param => {
+      if (param in query && !params.has(param))
+        params.set(param, query[param] as string)
+    })
+
+    // Return final URL with updated params
+    const updatedParams = params.toString()
+    return updatedParams ? `${path}?${updatedParams}` : path
+  }, [query, isAbsolute, href, retainParams])
+
+  return isAbsolute ? (
     <a
       target='_blank'
       rel='noreferrer noopener'
-      href={href}
+      href={hrefWithParams}
       download={download}
       className={className}
       onClick={onClick}
     >
       {children}
     </a>
+  ) : (
+    <Link href={hrefWithParams}>
+      <a className={className} onClick={onClick}>
+        {children}
+      </a>
+    </Link>
   )
 }
 
@@ -36,6 +70,8 @@ export interface SmartLinkProps {
   href?: string
   download?: string
   className?: string
+  retainExtraParams?: Array<string>
+  retainDefaultParams?: boolean
   onClick?: () => void
 }
 
@@ -47,10 +83,13 @@ export const SmartLinkPropTypes = {
   href: PropTypes.string,
   download: PropTypes.string,
   className: PropTypes.string,
+  retainExtraParams: PropTypes.arrayOf(PropTypes.string),
+  retainDefaultParams: PropTypes.bool,
   onClick: PropTypes.func
 }
 
 SmartLink.propTypes = SmartLinkPropTypes
 SmartLink.defaultProps = {
+  retainDefaultParams: true,
   onClick: () => {}
 }
