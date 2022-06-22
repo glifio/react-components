234944dd100ceb036e5e5ccd27ec6bb038dfe40a
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-22 13:57:48 +0200

    update url params

--- src/utils/urlParams/index.ts
+++ src/utils/urlParams/index.ts
@@ -1,84 +1,62 @@
 import { NextRouter } from 'next/router'
 
-const requiredUrlParamsWithDefaults = {}
-
-interface NavigationOptions {
-  existingQParams: Record<string, string | string[]>
-  pageUrl: string
-  newQueryParams?: Record<string, any>
-  maintainQueryParams?: boolean
-}
-
-export const combineExistingNewAndRequiredQueryParams = (
-  existingQParams: Record<string, string | string[]>,
-  newQParams?: Record<string, string>,
-  maintainQParams?: boolean
-): URLSearchParams => {
-  const searchParams = new URLSearchParams(
-    existingQParams as Record<string, string>
-  )
-
-  // delete q params if the maintainQParams flag is not set
-  if (!maintainQParams) {
-    for (const [key] of [...searchParams.entries()]) {
-      if (!requiredUrlParamsWithDefaults[key]) {
-        searchParams.delete(key)
-      }
-    }
-  }
-
-  // add new query params
-  if (newQParams) {
-    for (const param in newQParams) {
-      if (newQParams[param]) searchParams.set(param, newQParams[param])
-    }
-  }
-
-  // patch required q params if not present
-  for (const param in requiredUrlParamsWithDefaults) {
-    if (!searchParams.get(param)) {
-      searchParams.set(param, requiredUrlParamsWithDefaults[param])
-    }
-  }
-
-  return searchParams
-}
-
-export const generateRouteWithRequiredUrlParams = (
-  opts: NavigationOptions
-): string => {
-  let maintain: boolean = true
-  // default to maintain
-  if (typeof opts?.maintainQueryParams !== 'undefined')
-    maintain = opts.maintainQueryParams
-
-  const newParams = combineExistingNewAndRequiredQueryParams(
-    opts.existingQParams,
-    opts?.newQueryParams,
-    maintain
-  )
-
-  if (!newParams.toString()) {
-    return opts.pageUrl
-  }
-
-  return `${opts.pageUrl}?${newParams.toString()}`
-}
-
-// maintains the required query params while navigating to pageUrl
-export const navigate = (
-  router: NextRouter,
-  opts: Omit<NavigationOptions, 'existingQParams'>
+const appendParam = (
+  params: URLSearchParams,
+  key: string,
+  value: string | number
 ): void => {
-  router.push(
-    generateRouteWithRequiredUrlParams({
-      ...opts,
-      existingQParams: {}
-    })
-  )
+  const valueStr = typeof value === 'number' ? value.toString() : value
+  params.append(key, valueStr)
 }
 
-export const resetWallet = () => {
+export const appendParams = (
+  url: string,
+  params: Record<string, string | string[] | number | number[]>
+): string => {
+  // Get parameter string from input URL
+  const [path, paramString] = url.split('?')
+
+  // Create parameters object
+  const paramObject = new URLSearchParams(paramString)
+
+  // Add new parameters
+  Object.entries(params).forEach(([key, value]) => {
+    // Delete old value if exists
+    if (paramObject.has(key)) paramObject.delete(key)
+    // Append array values
+    if (Array.isArray(value))
+      value.forEach(v => appendParam(paramObject, key, v))
+    // Append single values
+    else appendParam(paramObject, key, value)
+  })
+
+  // Return URL with updated parameters
+  const updatedParams = params.toString()
+  return updatedParams ? `${path}?${updatedParams}` : path
+}
+
+interface NavigateOptions {
+  pageUrl: string
+  params?: Record<string, string | string[] | number | number[]>
+  retainParams?: boolean
+}
+
+export function navigate(
+  router: NextRouter,
+  { pageUrl, params, retainParams }: NavigateOptions
+): void {
+  let updatedUrl = pageUrl
+
+  // Add existing query params if retained
+  if (retainParams) updatedUrl = appendParams(updatedUrl, router.query)
+
+  // Add new query params if passed
+  if (params) updatedUrl = appendParams(updatedUrl, params)
+
+  router.push(updatedUrl)
+}
+
+export const resetWallet = (): void => {
   // perform a hard reload of the landing page without path and parameters
   window.location.assign(`${window.location.protocol}//${window.location.host}`)
 }
