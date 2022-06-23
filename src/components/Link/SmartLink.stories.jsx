18ad181e341c99f7be676a63eb2da7551b9a3f52
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-23 11:26:52 +0200

    Update SmartLink.stories.jsx

--- src/components/Link/SmartLink.stories.jsx
+++ src/components/Link/SmartLink.stories.jsx
@@ -28,3 +28,41 @@ Base.args = {
   href: 'https://google.com',
   name: 'Click here'
 }
+
+export const Internal1 = Template.bind({})
+Internal1.args = {
+  href: '/home',
+  name: 'Home'
+}
+
+export const Internal2 = Template.bind({})
+Internal2.args = {
+  href: 'admin',
+  name: 'Admin'
+}
+
+export const DownloadInternal = Template.bind({})
+DownloadInternal.args = {
+  href: '/favicon.ico',
+  download: true,
+  name: 'Download'
+}
+
+export const DownloadExternal = Template.bind({})
+DownloadExternal.args = {
+  href: 'http://localhost:6006/favicon.ico',
+  download: true,
+  name: 'Download'
+}
+
+export const MailTo = Template.bind({})
+MailTo.args = {
+  href: 'mailto:squad@glif.io',
+  name: 'squad@glif.io'
+}
+
+export const Tel = Template.bind({})
+Tel.args = {
+  href: 'tel:+4733378901',
+  name: '+4733378901'
+}
