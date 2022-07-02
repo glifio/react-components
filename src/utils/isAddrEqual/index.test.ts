17dc2b72ef8d3059603a386e58aed6c8e16c4e8e
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-07-02 00:25:51 -0700

    Improve isAddrEqual fn

---
+++ src/utils/isAddrEqual/index.test.ts
@@ -0,0 +1,38 @@
+import { isAddrEqual } from '.'
+import {
+  WALLET_ADDRESS,
+  WALLET_ID,
+  WALLET_ADDRESS_2,
+  WALLET_ID_2
+} from '../../test-utils/constants'
+
+describe('isAddrEqual', () => {
+  test('it accepts equal addresses', () => {
+    expect(
+      isAddrEqual(
+        { id: WALLET_ID, robust: WALLET_ADDRESS },
+        { id: WALLET_ID, robust: WALLET_ADDRESS }
+      )
+    ).toBe(true)
+    expect(
+      isAddrEqual({ id: WALLET_ID, robust: WALLET_ADDRESS }, WALLET_ID)
+    ).toBe(true)
+    expect(
+      isAddrEqual({ id: WALLET_ID, robust: WALLET_ADDRESS }, WALLET_ADDRESS)
+    ).toBe(true)
+  })
+  test('it rejects different addresses', () => {
+    expect(
+      isAddrEqual(
+        { id: WALLET_ID, robust: WALLET_ADDRESS },
+        { id: WALLET_ID_2, robust: WALLET_ADDRESS_2 }
+      )
+    ).toBe(false)
+    expect(
+      isAddrEqual({ id: WALLET_ID, robust: WALLET_ADDRESS }, WALLET_ID_2)
+    ).toBe(false)
+    expect(
+      isAddrEqual({ id: WALLET_ID, robust: WALLET_ADDRESS }, WALLET_ADDRESS_2)
+    ).toBe(false)
+  })
+})
