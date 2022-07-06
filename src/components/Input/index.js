0af76de2b7c3cc1fcb658b246b96de8e046de5ec
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-06 12:44:32 +0200

    Remove old Mnemonic input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,8 @@
-import Mnemonic from './Mnemonic'
 import { DenomTag, NumberInput, RawNumberInput } from './Number'
 import Text from './Text'
 import BaseInput from './BaseInput'
 
 export default {
-  Mnemonic,
   Number: NumberInput,
   RawNumberInput,
   Text,
