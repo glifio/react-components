862fdd6309f4296b06248f8f1d5a805d73dcf563
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-06 12:48:31 +0200

    Remove old text input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,9 @@
 import { DenomTag, NumberInput, RawNumberInput } from './Number'
-import Text from './Text'
 import BaseInput from './BaseInput'
 
 export default {
   Number: NumberInput,
   RawNumberInput,
-  Text,
   DenomTag,
   Base: BaseInput
 }
