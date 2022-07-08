1a45723b01a0ec0bc00e6fc628e2c6528af65830
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-07-08 15:20:20 -0700

    Add sync status to message history

---
+++ src/components/SyncStatus/index.tsx
@@ -0,0 +1,60 @@
+import { BigNumber } from '@glif/filecoin-number'
+import { useMemo, useState } from 'react'
+import dayjs from 'dayjs'
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+
+import {
+  useChainHeadSubscription,
+  useStatusQuery
+} from '../../generated/graphql'
+import { WarningBox } from '../Layout'
+
+dayjs.extend(relativeTime.default)
+
+export const SyncStatus = () => {
+  const [chainHead, setChainHead] = useState(0)
+  const { data } = useStatusQuery()
+
+  useChainHeadSubscription({
+    variables: {},
+    shouldResubscribe: chainHead === 0,
+    onSubscriptionData: ({ subscriptionData }) => {
+      if (
+        !subscriptionData.loading &&
+        !subscriptionData.error &&
+        chainHead === 0
+      ) {
+        setChainHead(subscriptionData.data?.chainHead?.height as number)
+      }
+    }
+  })
+
+  const syncPercentage = useMemo(() => {
+    if (data?.status?.height > 0 && chainHead > 0) {
+      return new BigNumber((chainHead - data?.status?.height) / chainHead)
+        .multipliedBy(100)
+        .toFixed(0)
+    }
+    return 0
+  }, [data?.status?.height, chainHead])
+
+  const estimate = useMemo(() => {
+    if (data?.status?.estimate > 0) {
+      return dayjs().add(data.status.estimate, 'seconds').fromNow()
+    }
+    return ''
+  }, [data?.status?.estimate])
+
+  return (
+    <WarningBox>
+      <h3>Sync in Progress</h3>
+      {syncPercentage > 0 && (
+        <p>
+          Our servers are syncing transactions from Filecoin Mainnet. We&apos;re
+          currently {syncPercentage}% synced.
+        </p>
+      )}
+      {!!estimate && <p>We expect to be done {estimate}</p>}
+    </WarningBox>
+  )
+}
