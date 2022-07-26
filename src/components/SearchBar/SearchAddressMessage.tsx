6f86b53e9f0ecf969b2987fe8472eb2626679e2d
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-26 11:48:54 +0200

    Create SearchAddressMessage.tsx

---
+++ src/components/SearchBar/SearchAddressMessage.tsx
@@ -0,0 +1,75 @@
+import { validateAddressString } from '@glif/filecoin-address'
+import PropTypes from 'prop-types'
+import { useCallback, useState } from 'react'
+import { SearchBar, SearchBarProps } from '.'
+import { validateCID } from '../../utils'
+
+/**
+ * SearchAddressMessage
+ */
+export const SearchAddressMessage = ({
+  large,
+  disabled,
+  onSearchAddress,
+  onSearchMessage
+}: SearchAddressMessageProps) => {
+  const [error, setError] = useState<string>('')
+  const [hasFocus, setHasFocus] = useState<boolean>(false)
+
+  const onSearch = useCallback((value: string) => {
+    setError('')
+    if (validateAddressString(value)) {
+      onSearchAddress(value)
+      return
+    }
+    if (validateCID(value)) {
+      onSearchMessage(value)
+      return
+    }
+    setError('Invalid search term')
+  }, [])
+
+  const onFocus = () => {
+    setHasFocus(true)
+  }
+
+  const onBlur = () => {
+    setHasFocus(false)
+  }
+
+  return (
+    <SearchBar
+      large={large}
+      error={!hasFocus ? error : ''}
+      name='search-address-message'
+      autoFocus={true}
+      autoComplete='on'
+      disabled={disabled}
+      placeholder={'Enter an address or message CID'}
+      onSearch={onSearch}
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />
+  )
+}
+
+export type SearchAddressMessageProps = {
+  large?: boolean
+  disabled?: boolean
+  onSearchAddress?: (address: string) => void
+  onSearchMessage?: (cid: string) => void
+}
+
+SearchAddressMessage.propTypes = {
+  large: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onSearchAddress: PropTypes.func,
+  onSearchMessage: PropTypes.func
+}
+
+ SearchAddressMessage.defaultProps = {
+  large: false,
+  disabled: false,
+  onSearchAddress: () => {},
+  onSearchMessage: () => {}
+}
