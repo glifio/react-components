c1a0363db6e0a9add2840179eea4e8aa4799ef93
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-07-26 12:07:30 -0700

    Fix import

--- src/components/AccountSelector/table.tsx
+++ src/components/AccountSelector/table.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { TABLE, TR, TH, TD } from '../HistoryTables/table'
+import { TABLE, TR, TH, TD } from '../Table'
 import { AddressLink } from '../AddressLink'
 import { space } from '../theme'
 import { Wallet } from '../../services/WalletProvider'
