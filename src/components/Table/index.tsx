b148716acc3c8a7872ed429e060a4bd9b3e36820
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-28 22:25:50 +0200

    Use caption styling from base-css

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,20 +1,4 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-
-export const CAPTION = styled.caption`
-  line-height: 1.5em;
-  text-align: left;
-  caption-side: bottom;
-  padding: 1em;
-  margin: 0.5em 0;
-  color: ${props => props.theme.colors.gray.medium};
-
-  &.error {
-    background: ${props => props.theme.colors.red.light};
-    color: ${props => props.theme.colors.red.dark};
-    border-radius: 4px;
-  }
-`
 
 export const TableCaption = ({
   name,
@@ -24,10 +8,10 @@ export const TableCaption = ({
 }: TableCaptionProps) => {
   if (error)
     return (
-      <CAPTION className='error'>{`${name} failed to load: ${error.message}`}</CAPTION>
+      <caption className='error'>{`${name} failed to load: ${error.message}`}</caption>
     )
-  if (loading) return <CAPTION>{`${name} is loading...`}</CAPTION>
-  if (empty) return <CAPTION>{`No records found for ${name}`}</CAPTION>
+  if (loading) return <caption>{`${name} is loading...`}</caption>
+  if (empty) return <caption>{`No records found for ${name}`}</caption>
   return <></>
 }
 
