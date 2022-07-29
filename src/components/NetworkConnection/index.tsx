976c5dbb9084c598afb551c671b8235206817e34
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-29 14:09:42 +0200

    Use status icon from layout

--- src/components/NetworkConnection/index.tsx
+++ src/components/NetworkConnection/index.tsx
@@ -4,57 +4,20 @@ import PropTypes from 'prop-types'
 import { useNetworkName } from './useNetworkName'
 import { useEffect, useMemo, useState } from 'react'
 import { useNetworkStatus } from './useNetworkStatus'
+import { StatusIcon } from '../Layout'
 
 export * from './useNetworkName'
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 0.4em;
+  gap: var(--space-s);
 `
 
-type Status = Partial<{
-  success: boolean
-  connecting: boolean
-  error: boolean
-}>
-
-const statusColor = ({ success, connecting, error }: Status) => {
+const getStatusColor = (success, error) => {
   if (success) return 'green'
-  if (connecting) return 'grey'
   if (error) return 'red'
-  return ''
-}
-
-const StatusOuter = styled.div`
-  position: relative;
-  width: 18px;
-  height: 18px;
-  border: 1px solid ${statusColor};
-  border-radius: 50%;
-`
-
-const StatusInner = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 11px;
-  height: 11px;
-  background: ${statusColor};
-  border-radius: 50%;
-  transform: translate(-50%, -50%);
-`
-
-const Status = (props: Status) => (
-  <StatusOuter {...props}>
-    <StatusInner {...props} />
-  </StatusOuter>
-)
-
-Status.defaultProps = {
-  connecting: true,
-  success: false,
-  error: false
+  return 'gray'
 }
 
 export function NetworkConnection({
@@ -94,7 +57,7 @@ export function NetworkConnection({
 
   return (
     <Wrapper>
-      <Status connecting={connecting} error={error} success={success} />
+      <StatusIcon color={getStatusColor(success, error)} />
       <span>{connecting ? 'Loading network' : networkName}</span>
     </Wrapper>
   )
