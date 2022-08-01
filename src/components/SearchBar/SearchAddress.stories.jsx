a8912960c6e46f87aa3444a76b91fbf1e9d5dae9
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-01 18:11:21 +0200

    Add SearchAddress component

---
+++ src/components/SearchBar/SearchAddress.stories.jsx
@@ -0,0 +1,24 @@
+import { SearchAddress } from './SearchAddress'
+import theme from '../theme'
+import ThemeProvider from '../ThemeProvider'
+
+export default {
+  title: 'SearchBar/SearchAddress',
+  component: SearchAddress,
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '35em', margin: '0 auto' }}>
+        <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
+      </div>
+    )
+  ],
+  parameters: { actions: { argTypesRegex: '^on.*' } }
+}
+
+const Template = args => <SearchAddress {...args} />
+
+export const Base = Template.bind({})
+Base.args = {}
+
+export const Large = Template.bind({})
+Large.args = { large: true }
