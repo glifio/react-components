cbe8e64133b16b8d8f046bdd2c5f6483200f3e28
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-22 14:34:29 +0200

    Create LabeledLink.tsx

---
+++ src/components/LabeledText/LabeledLink.tsx
@@ -0,0 +1,97 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+import { LabeledText } from '.'
+import { SmartLink } from '../SmartLink'
+import { CopyText } from '../Copy'
+import { IconNewTab } from '../Icons'
+
+const LabeledLinkEl = styled.div`
+  display: flex;
+  grid-gap: 0.25em;
+  line-height: 1.5;
+
+  > a {
+    color: ${props => props.color};
+    text-decoration: none;
+
+    &:hover {
+      color: ${props => props.color};
+      text-decoration: underline;
+    }
+
+    svg {
+      transition: 0.24s ease-in-out;
+      vertical-align: middle;
+
+      &:hover {
+        transform: scale(1.25);
+        fill: var(--purple-medium);
+      }
+    }
+  }
+`
+
+export const LabeledLink = ({
+  label,
+  color,
+  href,
+  linkText,
+  copyText,
+  disableLink,
+  hideCopy,
+  hideCopyText,
+  useNewTabIcon
+}: LabeledLinkProps) => {
+  return (
+    <LabeledText label={label}>
+      <LabeledLinkEl color={color}>
+        {disableLink || useNewTabIcon ? (
+          <span>{linkText}</span>
+        ) : (
+          <SmartLink href={href}>{linkText}</SmartLink>
+        )}
+        {useNewTabIcon && (
+          <SmartLink href={href}>
+            <IconNewTab />
+          </SmartLink>
+        )}
+        {!hideCopy && (
+          <CopyText text={copyText} hideCopyText={hideCopyText} color={color} />
+        )}
+      </LabeledLinkEl>
+    </LabeledText>
+  )
+}
+
+export interface LabeledLinkProps {
+  label?: string
+  color?: string
+  href: string
+  linkText?: string
+  copyText?: string
+  disableLink?: boolean
+  hideCopy?: boolean
+  hideCopyText?: boolean
+  useNewTabIcon?: boolean
+}
+
+LabeledLink.propTypes = {
+  label: PropTypes.string,
+  color: PropTypes.string,
+  href: PropTypes.string.isRequired,
+  linkText: PropTypes.string,
+  copyText: PropTypes.string,
+  disableLink: PropTypes.bool,
+  hideCopy: PropTypes.bool,
+  hideCopyText: PropTypes.bool,
+  useNewTabIcon: PropTypes.bool
+}
+
+LabeledLink.defaultProps = {
+  color: 'var(--purple-medium)',
+  disableLink: false,
+  hideCopy: false,
+  hideCopyText: true,
+  useNewTabIcon: false
+}
