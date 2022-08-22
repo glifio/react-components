06ea410c45f7ed46f169c1f82bd2bb072aeea44c
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-22 14:52:23 +0200

    Update AddressLink.tsx

--- src/components/LabeledText/AddressLink.tsx
+++ src/components/LabeledText/AddressLink.tsx
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types'
 import { useMemo } from 'react'
 
 import truncateAddress from '../../utils/truncateAddress'
-import { LabeledLink } from './LabeledLink'
+import {
+  LabeledLink,
+  LabeledLinkProps,
+  LabeledLinkPropTypes
+} from './LabeledLink'
 
 const explorerUrl =
   process.env.NEXT_PUBLIC_EXPLORER_URL ||
@@ -21,8 +25,7 @@ export const AddressLink = ({
 }: AddressLinkProps) => {
   // prioritize robust > id, use id if no robust exists
   const linkText = useMemo(() => {
-    if (address)
-      return shouldTruncate ? truncateAddress(address) : address
+    if (address) return shouldTruncate ? truncateAddress(address) : address
     return id || ''
   }, [address, id, shouldTruncate])
   const copyText = address || id
@@ -42,35 +45,22 @@ export const AddressLink = ({
   )
 }
 
-export interface AddressLinkProps {
+export type AddressLinkProps = {
   id?: string
   address?: string
-  label?: string
-  color?: string
-  disableLink: boolean
-  hideCopy: boolean
-  hideCopyText: boolean
   shouldTruncate?: boolean
-  useNewTabIcon?: boolean
-}
+} & Omit<LabeledLinkProps, 'href' | 'linkText' | 'copyText'>
+
+const { href, linkText, copyText, ...addressLinkPropTypes } =
+  LabeledLinkPropTypes
 
 AddressLink.propTypes = {
   id: PropTypes.string,
   address: PropTypes.string,
-  label: PropTypes.string,
-  color: PropTypes.string,
-  disableLink: PropTypes.bool,
-  hideCopy: PropTypes.bool,
-  hideCopyText: PropTypes.bool,
   shouldTruncate: PropTypes.bool,
-  useNewTabIcon: PropTypes.bool
+  ...addressLinkPropTypes
 }
 
 AddressLink.defaultProps = {
-  color: 'var(--purple-medium)',
-  disableLink: false,
-  hideCopy: false,
-  hideCopyText: true,
-  shouldTruncate: true,
-  useNewTabIcon: false
+  shouldTruncate: true
 }
