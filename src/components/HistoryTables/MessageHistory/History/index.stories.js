bea91c3ade76801ce6ccdc260664d465ddbafa3a
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-08-23 20:10:49 -0700

    Fix message history story

--- src/components/HistoryTables/MessageHistory/History/index.stories.js
+++ src/components/HistoryTables/MessageHistory/History/index.stories.js
@@ -1,29 +1,13 @@
-import { CoinType } from '@glif/filecoin-address'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../../apolloClient'
-import theme from '../../../theme'
-import ThemeProvider from '../../../ThemeProvider'
 
 import MessageHistory from './index'
-import { Environment, Network } from '../../../../services/EnvironmentProvider'
 
 export default {
   title: 'MessageHistory/Table',
   component: MessageHistory,
   decorators: [
-    Story => (
-      <Environment
-        coinType={CoinType.Test}
-        networkName={Network.CALIBRATION}
-        nodeStatusApiKey='m787669344-2a9b90eb03dbff3e503c93c7'
-        graphUrl='graph-calibration.glif.link/query'
-        lotusApiUrl='https://api.calibration.node.glif.io/'
-      >
-        <ApolloProvider client={client}>
-          <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
-        </ApolloProvider>
-      </Environment>
-    )
+    Story => <ApolloProvider client={client}>{Story()}</ApolloProvider>
   ],
   parameters: { actions: { argTypesRegex: '^on.*' } }
 }
