680aa093da4b013f58d6351cd258e202dab4dea6
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-30 15:35:03 +0200

    Simplify useStateReadState

--- src/utils/useStateReadState/index.ts
+++ src/utils/useStateReadState/index.ts
@@ -1,46 +1,22 @@
 import { useEffect, useMemo, useState } from 'react'
-import type { CID } from '@glif/filecoin-wallet-provider'
+import { LotusActorState } from '@glif/filecoin-actor-utils'
+import { validateAddressString } from '@glif/filecoin-address'
 import LotusRPCEngine from '@glif/filecoin-rpc-client'
 
-import { Address } from '../../generated/graphql'
 import { useEnvironment } from '../../services/EnvironmentProvider'
 
-export type LotusRPCActorState<T = object | null> = {
-  Balance: string
-  Code: CID
-  State: T
-}
-
-export type MsigState<T = Address> = {
-  InitialBalance: string
-  AvailableBalance: string
-  NextTxnID: number
-  NumApprovalsThreshold: number
-  Signers: T[]
-  StartEpoch: number
-  UnlockDuration: number
-}
-
-const emptyActorState: LotusRPCActorState<any> = {
-  Balance: '0',
-  Code: { '/': '' },
-  State: null
-}
-
 interface UseStateReadStateResult<T> {
-  data: LotusRPCActorState<T> | null
+  data: LotusActorState<T> | null
   loading: boolean
   error: Error | null
 }
 
-/////// implemented until https://github.com/glifio/graph/issues/33
 export const useStateReadState = <T = object | null>(
   address: string
 ): UseStateReadStateResult<T> => {
-  const [data, setData] = useState<LotusRPCActorState<T> | null>(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<Error>(undefined)
-  const [fetchedFor, setFetchedFor] = useState<string>('')
+  const [data, setData] = useState<LotusActorState<T> | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<Error | null>(null)
   const { lotusApiUrl: apiAddress } = useEnvironment()
 
   const lotusRPC = useMemo(
@@ -52,34 +28,20 @@ export const useStateReadState = <T = object | null>(
   )
 
   useEffect(() => {
-    const fetchState = async () => {
-      try {
-        const res = await lotusRPC.request<LotusRPCActorState<any>>(
-          'StateReadState',
-          address,
-          null
-        )
-        setData({ ...res })
-      } catch (err) {
-        if (err instanceof Error) {
-          if (err.message.includes('actor not found')) {
-            setData({ ...emptyActorState })
-          } else setError(err)
-        } else setError(new Error('Failed to fetch'))
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    const firstLoad = !data && loading && !error
-    const newLoad = fetchedFor !== address && !error
-
-    if (firstLoad || newLoad) {
+    setData(null)
+    setError(null)
+    if (!address) return
+    if (validateAddressString(address)) {
       setLoading(true)
-      setFetchedFor(address)
-      fetchState()
+      lotusRPC
+        .request<LotusActorState<T>>('StateReadState', address, null)
+        .then((a: LotusActorState<T>) => setData(a))
+        .catch((e: Error) => setError(e))
+        .finally(() => setLoading(false))
+    } else {
+      setError(new Error('Invalid address'))
     }
-  }, [address, loading, error, data, fetchedFor, setFetchedFor, lotusRPC])
+  }, [address, lotusRPC])
 
   return { data, error, loading }
 }
