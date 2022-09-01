2efdb8f7d2e5a6cbcf73e519998275fad8856fc7
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-09-01 12:22:34 -0700

    Clean up useAge hook

--- src/components/HistoryTables/MessageHistory/hooks/useAge.ts
+++ src/components/HistoryTables/MessageHistory/hooks/useAge.ts
@@ -1,8 +1,12 @@
-import dayjs, { Dayjs } from 'dayjs'
+import dayjs from 'dayjs'
 import { MessageConfirmedRow, MessagePendingRow } from '../../types'
 import { useTipsetQuery } from '../../../../generated/graphql'
 
-export const useUnformattedDateTime = (message: AgeHookArgs): Dayjs | null => {
+export const useAge = (
+  message: (MessageConfirmedRow | MessagePendingRow) & {
+    height?: number | string
+  }
+): string => {
   const { data: tipsetData } = useTipsetQuery({
     variables: {
       height: Number(message?.height)
@@ -10,16 +14,9 @@ export const useUnformattedDateTime = (message: AgeHookArgs): Dayjs | null => {
     skip: !message || !message?.height
   })
 
-  if (!tipsetData?.tipset?.minTimestamp) return null
+  if (!tipsetData?.tipset?.minTimestamp) return '...'
 
-  return dayjs.unix(tipsetData.tipset.minTimestamp)
-}
-
-export const useAge = (message: AgeHookArgs): string => {
-  const unformattedTime = useUnformattedDateTime(message)
-  return unformattedTime ? unformattedTime.format('YYYY-MM-DD hh:mm:ss') : '...'
-}
-
-type AgeHookArgs = (MessageConfirmedRow | MessagePendingRow) & {
-  height?: number | string
+  return dayjs
+    .unix(tipsetData.tipset.minTimestamp)
+    .format('YYYY-MM-DD hh:mm:ss')
 }
