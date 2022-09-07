b88e0e886101174bfdc073795a4812873ef201a0
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-07 20:46:22 +0200

    linter

--- src/utils/makeFriendlyBalance/index.ts
+++ src/utils/makeFriendlyBalance/index.ts
@@ -1,6 +1,9 @@
 import { BigNumber, FilecoinNumber } from '@glif/filecoin-number'
 
-export const makeFriendlyBalance = (value: FilecoinNumber, decimals: number = 3): string => {
+export const makeFriendlyBalance = (
+  value: FilecoinNumber,
+  decimals: number = 3
+): string => {
   // Invalid
   if (!FilecoinNumber.isFilecoinNumber(value)) {
     throw new Error('Can only make friendly balance for FilecoinNumber value')
@@ -21,25 +24,27 @@ export const makeFriendlyBalance = (value: FilecoinNumber, decimals: number = 3)
 
   // Zero after stripping decimals
   if (dpValue.isZero()) {
-    return `< 0.${minus}${Array(decimals - 1).fill('0').join('')}1`
+    return `< 0.${minus}${Array(decimals - 1)
+      .fill('0')
+      .join('')}1`
   }
 
   // Less than 1000
   if (absValue.isLessThan(1000)) {
-    return minus + absValue.decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString()
+    return (
+      minus + absValue.decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString()
+    )
   }
 
   // 1K to 1B
   let power = 0
   const units = ['K', 'M', 'B']
   for (const unit of units) {
-    power++;
+    power++
     const unitVal = dpValue.dividedBy(Math.pow(1000, power))
     if (unitVal.isLessThan(1000))
       return (
-        minus +
-        unitVal.decimalPlaces(3, BigNumber.ROUND_DOWN).toString() +
-        unit
+        minus + unitVal.decimalPlaces(3, BigNumber.ROUND_DOWN).toString() + unit
       )
   }
 
