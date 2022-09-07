d828e78fdcbe07d23968909c28b3f61466b3a9d9
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-07 20:18:01 +0200

    Update makeFriendlyBalance

--- src/utils/makeFriendlyBalance/index.ts
+++ src/utils/makeFriendlyBalance/index.ts
@@ -1,65 +1,47 @@
-import { BigNumber } from '@glif/filecoin-number'
+import { BigNumber, FilecoinNumber } from '@glif/filecoin-number'
 
-const makeFriendly = (bigNumber, denom) => {
-  const stringifiedNumber = bigNumber.toFixed(0)
-  let idx = Number(stringifiedNumber.length) % 3
-  if (idx === 0) idx = 3
-  const base = stringifiedNumber.slice(0, idx)
-  const dangler = stringifiedNumber.slice(idx, idx + 3)
-  return `${base}.${dangler}${denom}`
-}
+export const makeFriendlyBalance = (value: FilecoinNumber, decimals: number = 3): string => {
+  // Invalid
+  if (!FilecoinNumber.isFilecoinNumber(value)) {
+    throw new Error('Can only make friendly balance for FilecoinNumber value')
+  }
 
-const addComparisonOperator = (endNum, originalNum) => {
-  if (new BigNumber(endNum).isNaN()) return endNum
-  if (new BigNumber(originalNum).isEqualTo(new BigNumber(endNum))) return endNum
-  if (new BigNumber(originalNum).isLessThan(new BigNumber(endNum)))
-    return `< ${endNum}`
-  if (new BigNumber(originalNum).isGreaterThan(new BigNumber(endNum)))
-    return endNum
-}
+  // NaN
+  if (value.isNaN()) {
+    throw new Error('Cannot make friendly balance for NaN value')
+  }
 
-export default (
-  bigNumber: BigNumber | string | number,
-  dp: number = 3
-): BigNumber | string | number => {
-  if (!bigNumber) throw new Error('No number passed to big number')
-  if (!BigNumber.isBigNumber(bigNumber)) return bigNumber
-  if (bigNumber.isEqualTo(0)) return '0'
-  if (bigNumber.toString() === 'NaN') {
-    throw new Error('Number must be a valid number')
-  }
-  if (bigNumber.isLessThan(0)) throw new Error('Cannot have a negative balance')
-  if (bigNumber.isGreaterThan(0) && bigNumber.isLessThanOrEqualTo(1)) {
-    if (bigNumber.dp(dp, BigNumber.ROUND_DOWN).isEqualTo('0')) {
-      let abbrev = '0.'
-      for (let i = 0; i < dp - 1; i += 1) {
-        abbrev += '0'
-      }
-      return addComparisonOperator(`${abbrev}1`, bigNumber)
-    }
+  // Zero
+  if (value.isZero()) return '0'
 
-    return addComparisonOperator(
-      bigNumber.dp(dp, BigNumber.ROUND_DOWN).toString(),
-      bigNumber
-    )
+  const isNegative = value.isNegative()
+  const minus = isNegative ? '-' : ''
+  const absValue = value.absoluteValue()
+  const dpValue = absValue.decimalPlaces(decimals, BigNumber.ROUND_DOWN)
+
+  // Zero after stripping decimals
+  if (dpValue.isZero()) {
+    return `< ${minus}${Array(decimals - 1).fill('0').join('')}1`
   }
-  if (bigNumber.isGreaterThan(1) && bigNumber.isLessThanOrEqualTo(1000)) {
-    return bigNumber.dp(dp, BigNumber.ROUND_DOWN).toString()
+
+  // Less than 1000
+  if (absValue.isLessThan(1000)) {
+    return minus + absValue.decimalPlaces(decimals, BigNumber.ROUND_DOWN).toString()
   }
-  if (bigNumber.isGreaterThan(1000) && bigNumber.isLessThan(1000000)) {
-    return makeFriendly(bigNumber, 'K')
+
+  // 1K to 1B
+  let power = 0
+  const units = ['K', 'M', 'B']
+  for (const unit of units) {
+    power++;
+    const unitVal = dpValue.dividedBy(Math.pow(1000, power))
+    if (unitVal.isLessThan(1000))
+      return (
+        minus +
+        unitVal.decimalPlaces(3, BigNumber.ROUND_DOWN).toString() +
+        unit
+      )
   }
-  if (
-    bigNumber.isGreaterThanOrEqualTo(1000000) &&
-    bigNumber.isLessThan(1000000000)
-  ) {
-    return makeFriendly(bigNumber, 'M')
-  }
-  if (
-    bigNumber.isGreaterThanOrEqualTo(1000000000) &&
-    bigNumber.isLessThan(1000000000000)
-  ) {
-    return makeFriendly(bigNumber, 'B')
-  }
-  return '> 999.9B'
+
+  return isNegative ? '< -999.9B' : '> 999.9B'
 }
