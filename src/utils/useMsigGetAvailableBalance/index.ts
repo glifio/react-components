a75374974594c6fd62b30bd826c8c921b40431e9
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-13 10:28:48 +0200

    Update useMsigGetAvailableBalance

--- src/utils/useMsigGetAvailableBalance/index.ts
+++ src/utils/useMsigGetAvailableBalance/index.ts
@@ -1,5 +1,5 @@
 import useSWR, { SWRConfiguration } from 'swr'
-import { useCallback } from 'react'
+import LotusRpcEngine from '@glif/filecoin-rpc-client'
 import { FilecoinNumber } from '@glif/filecoin-number'
 import { validateAddressString } from '@glif/filecoin-address'
 
@@ -15,28 +15,25 @@ export const useMsigGetAvailableBalance = (
   address: string,
   swrConfig: SWRConfiguration = { refreshInterval: 10000 }
 ): UseMsigGetAvailableBalanceResult => {
+  const fetcher = async (
+    lotusApi: LotusRpcEngine,
+    lotusMethod: string,
+    actorAddress: string
+  ): Promise<FilecoinNumber | null> => {
+    if (!actorAddress) return null
+
+    if (!validateAddressString(actorAddress))
+      throw new Error('Invalid actor address')
+
+    return new FilecoinNumber(
+      await lotusApi.request<string>(lotusMethod, actorAddress, null),
+      'attofil'
+    )
+  }
+
   const { lotusApi } = useEnvironment()
-
-  const fetcher = useCallback(
-    async (
-      actorAddress: string,
-      lotusMethod: string
-    ): Promise<FilecoinNumber | null> => {
-      if (!actorAddress) return null
-
-      if (!validateAddressString(actorAddress))
-        throw new Error('Invalid actor address')
-
-      return new FilecoinNumber(
-        await lotusApi.request<string>(lotusMethod, actorAddress, null),
-        'attofil'
-      )
-    },
-    [lotusApi]
-  )
-
   const { data, error } = useSWR<FilecoinNumber, Error>(
-    [address, 'MsigGetAvailableBalance'],
+    [lotusApi, 'MsigGetAvailableBalance', address],
     fetcher,
     swrConfig
   )
