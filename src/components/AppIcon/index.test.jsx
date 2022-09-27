d816770d14f4cb061a92d78db9f9a8ba290c4dcf
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-27 13:56:17 +0200

    Remove ThemeProvider / theme

--- src/components/AppIcon/index.test.jsx
+++ src/components/AppIcon/index.test.jsx
@@ -1,16 +1,10 @@
 import { render } from '@testing-library/react'
 
-import ThemeProvider from '../ThemeProvider'
-import theme from '../theme'
 import AppIcon from './index'
 
 describe('AppIcon', () => {
   test('renders the AppIcon', () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <AppIcon />
-      </ThemeProvider>
-    )
+    const { container } = render(<AppIcon />)
     expect(container.firstChild).toMatchSnapshot()
   })
 })
