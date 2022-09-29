d4e4f48d347c44ea7d0359fbeb4daff6c3336964
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-29 18:00:04 +0200

    Remove unused import

--- src/components/Connect/MetaMask/index.tsx
+++ src/components/Connect/MetaMask/index.tsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { FC, useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { IconMetaMaskFlask } from '../../Icons'
 import { HelperText } from './Helper'
