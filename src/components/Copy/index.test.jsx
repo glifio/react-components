6514f1b5f2078c13021a105a1cab1df5ba15ccee
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-29 14:47:25 +0200

    Remove CopyAddress

--- src/components/Copy/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { Base } from './index.stories'
-
-describe('CopyAddress', () => {
-  test('renders the story', () => {
-    const { container } = render(<Base {...Base.args} />)
-    expect(container.firstChild).toMatchSnapshot()
-  })
-})
