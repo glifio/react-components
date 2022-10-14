88f593bb2e78ec1001e072e25fe98ca6c46a33e8
Thijs van Hoof <thijs.v.h@gmail.com>
2022-10-14 17:44:00 -0500

    Remove PROPOSAL_ROW_PROP_TYPE

--- src/components/HistoryTables/types.ts
+++ src/components/HistoryTables/types.ts
@@ -15,12 +15,3 @@ export const MESSAGE_CONFIRMED_ROW_PROP_TYPE = PropTypes.shape({
   to: GRAPHQL_ADDRESS_PROP_TYPE.isRequired,
   value: PropTypes.string.isRequired
 })
-
-export const PROPOSAL_ROW_PROP_TYPE = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  method: PropTypes.number.isRequired,
-  params: PropTypes.object,
-  to: GRAPHQL_ADDRESS_PROP_TYPE,
-  approved: PropTypes.arrayOf(GRAPHQL_ADDRESS_PROP_TYPE),
-  proposalHash: PropTypes.string.isRequired
-})
