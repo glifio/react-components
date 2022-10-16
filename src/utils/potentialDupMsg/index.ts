64d41ab6a3d24c4dae4179d4c9c87fc80725b703
Thijs van Hoof <thijs.v.h@gmail.com>
2022-10-16 11:49:36 -0500

    Update index.ts

--- src/utils/potentialDupMsg/index.ts
+++ src/utils/potentialDupMsg/index.ts
@@ -13,10 +13,9 @@ export const potentialDupMsg = (
 
   const fromAddrEq = isAddrEqual(msg1.from, msg2.from)
   const toAddrEq = isAddrEqual(msg1.to, msg2.to)
-  const methodEq = Number(msg1.method) === msg2.method
-  const valueEq = new BigNumber(msg1.value).isEqualTo(msg2.value)
-  const paramsEq =
-    !msg1.params && !msg2.params ? true : msg1.params === msg2.params
+  const methodEq = msg1.method === msg2.method
+  const valueEq = msg2.value.isEqualTo(msg1.value)
+  const paramsEq = msg1.params === msg2.params
   // if the message is pending or occurred in the last 900 epochs, it's considered "recent"
   const isRecent =
     isPending || (msg1.height > 0 && currentHeight - msg1.height < 900)
