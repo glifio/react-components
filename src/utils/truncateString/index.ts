a237f532eb0a3a0d1f2799cebc432cf90c73c817
Thijs van Hoof <thijs.v.h@gmail.com>
2022-10-26 23:33:05 +0200

    Linter fix

--- src/utils/truncateString/index.ts
+++ src/utils/truncateString/index.ts
@@ -1,4 +1,8 @@
-export const truncateString = (value: string, begin: number = 6, end: number = 6): string =>
+export const truncateString = (
+  value: string,
+  begin: number = 6,
+  end: number = 6
+): string =>
   value.length <= begin + end
     ? value
     : `${value.slice(0, begin)} ... ${value.slice(-end)}`
