eff4c2c28b410a9300af8a5e22557cd71e6b6e80
Thijs van Hoof <thijs.v.h@gmail.com>
2022-11-16 11:21:11 +0100

    Update index.ts

--- src/utils/isFEVMTx/index.ts
+++ src/utils/isFEVMTx/index.ts
@@ -9,7 +9,7 @@ const isDelegatedWithNamespace = (
     ? address.slice(1).startsWith(`${Protocol.DELEGATED}${namespace}`)
     : false
 
-// Assumption: any actor that has an ID address and _no_ robust address is NOT an FEVM actor
+// Assumption: any actor that has an ID address and NO robust address is NOT an FEVM actor
 // Either the to or the from address must be an eth delegated address for this to return true
 export const isFEVMTx = (message: GqlMessage | GqlMessagePending): boolean =>
   message
