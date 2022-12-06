d883c9fff531a6a51e3e28bdff35a7b6920885b2
Thijs van Hoof <thijs.v.h@gmail.com>
2022-12-06 16:06:14 +0100

    Update useIsFEVMActor.ts

--- src/utils/useIsFEVMActor.ts
+++ src/utils/useIsFEVMActor.ts
@@ -1,55 +1,44 @@
-import {
-  decode,
-  ethAddressFromDelegated,
-  Protocol
-} from '@glif/filecoin-address'
 import useSWRImmutable from 'swr/immutable'
 import LotusRpcEngine from '@glif/filecoin-rpc-client'
+import { ethAddressFromDelegated } from '@glif/filecoin-address'
+
 import { useEnvironment } from '../services/EnvironmentProvider'
-import { isEthAddress } from './isAddress'
+import { isEthAddress, isDelegatedAddress } from './isAddress'
 
 const fetcher = async (
   apiAddress: string,
   lotusMethod: string,
-  address: string,
-  latest: string
-): Promise<any> => {
-  if (!apiAddress || !address) {
-    return false
-  } else if (
-    !isEthAddress(address) &&
-    decode(address).protocol() !== Protocol.DELEGATED
-  ) {
-    return false
-  }
+  actorAddress: string
+): Promise<number | null> => {
+  if (!apiAddress || !actorAddress) return null
 
-  let addrToFetch
-  try {
-    addrToFetch = ethAddressFromDelegated(address)
-  } catch {
-    addrToFetch = address
-  }
+  const addressToFetch = isEthAddress(actorAddress)
+    ? actorAddress
+    : isDelegatedAddress(actorAddress)
+    ? ethAddressFromDelegated(actorAddress)
+    : null
+
+  if (!addressToFetch) throw new Error('Invalid FEVM actor address')
 
   const lotusApi = new LotusRpcEngine({
     apiAddress,
     namespace: 'eth',
     delimeter: '_'
   })
-
-  // get the bytecode associated with this eth addr
-  const code = await lotusApi.request(lotusMethod, addrToFetch, latest)
-  // if bytecode exists, this is a smart contract
-  return code !== 0x00
+  return await lotusApi.request<number>(lotusMethod, addressToFetch, 'latest')
 }
 
-// given an address, return true if this represents an FEVM actor
-// TODO: we should just be able to match the actor's Code with the actorCode
+// Given an address, return true if this represents an FEVM actor
+// TODO: We should just be able to match the actor's Code with the actorCode
 export const useIsFEVMActor = (address: string): boolean => {
   const { lotusApiUrl } = useEnvironment()
-  const { data } = useSWRImmutable<boolean, Error>(
-    [lotusApiUrl, 'getCode', address, 'latest'],
+
+  // Get the bytecode associated with this address
+  const { data } = useSWRImmutable<number, Error>(
+    [lotusApiUrl, 'getCode', address],
     fetcher
   )
 
-  return data || false
+  // If the bytecode exists, this is a smart contract
+  return !!data
 }
